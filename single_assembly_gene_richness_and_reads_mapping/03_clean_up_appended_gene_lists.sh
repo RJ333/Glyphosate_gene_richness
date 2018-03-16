d0436fa815a9eefc4ff926e53738da20196891e6
RJ333 <35432752+RJ333@users.noreply.github.com>
2018-03-17 00:23:57 +0100

    updating workflow for #20

--- single_assembly_gene_richness_and_reads_mapping/03_clean_up_appended_gene_lists.sh
+++ single_assembly_gene_richness_and_reads_mapping/03_clean_up_appended_gene_lists.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-awk 'BEGIN { OFS = "\t" } ; {split ($1, a, "_"); split (a[3], b, "\\."); print $2, a[2], b[1]}' test_file.tsv
+# BEGIN adds table header and sets Output field sep to tab
+# split adresses specific column and splits on "_", storing the pieces in array a
+# from this leftover, another split is performed to remove the ".tsv", stored in array b
+# the respective fields are printed
+awk 'BEGIN { OFS = "\t" ; print "contig\tgene\tsample\treads_per_gene"}
+  {
+  split ($1, a, "_") 
+  split (a[3], b, "\\.") 
+  print $2, a[2], b[1], $3
+  }' appended_genes*tsv > contig_gene_sample_reads.tsv
---
+++ single_assembly_gene_richness_and_reads_mapping/04_calculate_gene_richness_and_abundance.r
@@ -0,0 +1,70 @@
+library(ggplot2)
+library(data.table)
+
+# read in mapping file
+gene_reads <- read.delim(file.choose(), header = TRUE)  # contigs_genes_sample_reads.tsv
+
+# generate meta data and merge with mapping file
+sample <- c("A1", "A2", "A3", "A4", "A5", "A6", "A7", "B8", "B9", "B10")
+days <- c(0, 3, 7, 14, 22, 43, 71, 0, 22, 71)
+treatment <- c("glyph", "glyph", "glyph", "glyph", "glyph", "glyph", "glyph", "control", "control", "control")
+sample_meta_data <- data.frame(sample, days, treatment)
+
+gene_reads2 <- merge(gene_reads, sample_meta_data, by = "sample")
+
+# calculating richness data per gene
+gene_richness <- aggregate( . ~  gene + sample + days + treatment, data = gene_reads2, length)
+gene_richness <- gene_richness[with(gene_richness, order(gene)), ]
+gene_richness <- gene_richness[,c(1:5)]
+colnames(gene_richness)[colnames(gene_richness)=="contig_id"] <- "gene_richness"
+
+
+# add column with relative richness values with data.table
+gene_richness <- setDT(gene_richness)
+gene_richness[,gene_richness_relative := gene_richness/gene_richness[days == 0]*100, by = .(gene,treatment)]
+
+# create subsets with and without glyphosate related genes
+phn_operon_richness_subset <- subset(gene_richness, grepl("phn[C-P]|sox[A-B]", gene))
+ref_richness_subset <- subset(gene_richness, !grepl("phn[C-P]|sox[A-B]", gene))
+
+# plot subsets
+ggplot(phn_operon_richness_subset, aes(x = days, y = gene_richness_relative, group = gene, colour = gene)) +
+  geom_line(size = 2) +
+  geom_text(aes(label = gene), show.legend = FALSE) +
+  facet_grid(~ treatment)
+  
+ggplot(ref_richness_subset, aes(x = days, y = gene_richness_relative, group = gene, colour = gene)) +
+  geom_line(size = 2) +
+  geom_text(aes(label = gene), show.legend = FALSE) +
+  facet_grid(~ treatment)
+
+
+# generating read abundance data per gene
+gene_abundance <- aggregate( . ~  gene + sample + days + treatment, data = gene_reads2, sum)
+gene_abundance <- gene_abundance[,c(1:4,6)]
+gene_abundance <- gene_abundance[with(gene_abundance, order(gene)), ]
+
+# add column with relative richness values
+gene_abundance <- setDT(gene_abundance)
+gene_abundance[,reads_per_gene_relative := reads_per_gene/reads_per_gene[days == 0]*100, by = .(gene,treatment)]
+
+# create subsets
+phn_operon_abundance_subset <- subset(gene_abundance, grepl("phn[C-P]|sox[A-B]", gene))
+ref_abundance_subset <- subset(gene_abundance, !grepl("phn[C-P]|sox[A-B]", gene))
+
+# plot subsets
+ggplot(phn_operon_abundance_subset, aes(x = days, y = reads_per_gene_relative, group = gene, colour = gene)) +
+  geom_line(size = 1.5) +
+  geom_text(aes(label = gene), show.legend = FALSE) +
+  facet_grid(~ treatment)
+
+ggplot(ref_abundance_subset, aes(x = days, y = reads_per_gene_relative, group = gene, colour = gene)) +
+  coord_cartesian(ylim = c(0,400)) +
+  geom_line(size = 1.5) +
+  geom_text(aes(label = gene), show.legend = FALSE) +
+  facet_grid(~ treatment)
+
+
+
+
+
