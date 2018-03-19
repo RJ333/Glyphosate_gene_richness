0ac5cea255ef4c678c93898ee4e867a81dc887cd
RJ333 <35432752+RJ333@users.noreply.github.com>
2018-03-19 14:16:03 +0100

    combined step 2 and 3 in one script #20

--- single_assembly_gene_richness_and_reads_mapping/02_append_and_clean_samtools_view_output.sh
+++ single_assembly_gene_richness_and_reads_mapping/02_append_and_clean_samtools_view_output.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
 
+# puts all single files into one file
+
+for nfiles in named_*.tsv
+do	
+  cat $nfiles >> appended_genes.tsv
+  echo "appending $nfiles done"
+done
+
+echo "cleaning up table"
+
 # BEGIN adds table header and sets Output field sep to tab
 # split adresses specific column and splits on "_", storing the pieces in array a
 # from this leftover, another split is performed to remove the ".tsv", stored in array b
 # the respective fields are printed
+
 awk 'BEGIN { OFS = "\t" ; print "contig\tgene\tsample\treads_per_gene"}
   {
   split ($1, a, "_") 
   split (a[3], b, "\\.") 
   print $2, a[2], b[1], $3
   }' appended_genes*tsv > contig_gene_sample_reads.tsv
+  
+rm appended_genes*tsv
+
+echo "cleaning is done, output written to contig_gene_sample_reads.tsv"
--- single_assembly_gene_richness_and_reads_mapping/02_append_samtools_view_output.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-for nfiles in named_*.tsv
-do	
-  echo $nfiles
-  cat $nfiles >> appended_genes.tsv
-done
