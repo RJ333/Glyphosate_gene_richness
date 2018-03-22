83511f94912797f2bba34dea0ff7f865646e876b
RJ333 <35432752+RJ333@users.noreply.github.com>
2018-03-22 19:37:49 +0100

    fixing files #20

--- single_assembly_gene_richness_and_reads_mapping/02_append_and_clean_samtools_view_output.sh
+++ single_assembly_gene_richness_and_reads_mapping/02_append_and_clean_samtools_view_output.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-<<<<<<< HEAD:single_assembly_gene_richness_and_reads_mapping/03_clean_up_appended_gene_lists.sh
 # BEGIN adds table header and sets output field sep to tab
 # split() adresses specific column and splits on "_", storing the pieces in array a
 # from this leftover a[3], another split is performed to remove the ".tsv", stored in array b
-=======
 # puts all single files into one file
 
 for nfiles in named_*.tsv
@@ -18,7 +16,6 @@ echo "cleaning up table"
 # BEGIN adds table header and sets Output field sep to tab
 # split adresses specific column and splits on "_", storing the pieces in array a
 # from this leftover, another split is performed to remove the ".tsv", stored in array b
->>>>>>> a6bfaabca3ef6b582bbbf357bdd366aaa9e3ef70:single_assembly_gene_richness_and_reads_mapping/02_append_and_clean_samtools_view_output.sh
 # the respective fields are printed
 
 awk 'BEGIN { OFS = "\t" ; print "contig\tgene\tsample\treads_per_gene"}
