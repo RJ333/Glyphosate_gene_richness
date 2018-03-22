cdc0ceb489f25105f41181e674c308e1c106d36f
RJ333 <35432752+RJ333@users.noreply.github.com>
2018-03-22 19:29:45 +0100

    #20 another update

--- single_assembly_gene_richness_and_reads_mapping/03_calculate_gene_richness_and_abundance.r
+++ single_assembly_gene_richness_and_reads_mapping/03_calculate_gene_richness_and_abundance.r
@@ -22,7 +22,7 @@ gene_reads2 <- merge(gene_reads, sample_meta_data, by = "sample")
 gene_richness <- aggregate( . ~  gene + sample + days + treatment, data = gene_reads2, length)
 gene_richness <- gene_richness[with(gene_richness, order(gene)), ]
 gene_richness <- gene_richness[,c(1:5)]
-colnames(gene_richness)[colnames(gene_richness)=="contig_id"] <- "gene_richness"
+colnames(gene_richness)[colnames(gene_richness)=="contig"] <- "gene_richness"
 
 
 # add column with relative richness values with data.table
@@ -30,22 +30,40 @@ gene_richness <- setDT(gene_richness)
 gene_richness[,gene_richness_relative := gene_richness/gene_richness[days == 0]*100, by = .(gene,treatment)]
 
 # create subsets with and without glyphosate related genes
-phn_operon_richness_subset <- subset(gene_richness, grepl("phn[C-P]|sox[A-B]", gene))
+phn_operon_richness_subset <- subset(gene_richness, grepl("phn[C-P]", gene))
+phn_operon_richness_subset_glyph <- subset(gene_richness, grepl("phn[C-P]", gene) & treatment == "glyph")
+sox_richness_subset <- subset(gene_richness, grepl("sox[A-Z]", gene))
 ref_richness_subset <- subset(gene_richness, !grepl("phn[C-P]|sox[A-B]", gene))
+ref_richness_subset_glyph <- subset(gene_richness, !grepl("phn[C-P]|sox[A-B]", gene) & treatment == "glyph")
+
+nitrogen_rich_subset <- subset(gene_richness, grepl("nirS|norB|nosZ|nuoF", gene))
 
 # plot subsets
+ggplot(nitrogen_rich_subset, aes(x = days, y = gene_richness, group = gene, colour = gene)) +
+  geom_line(size = 2) +
+  geom_text(aes(label = gene), show.legend = FALSE) +
+  facet_grid(~ treatment)
+
 ggplot(phn_operon_richness_subset, aes(x = days, y = gene_richness_relative, group = gene, colour = gene)) +
   geom_line(size = 2) +
   geom_text(aes(label = gene), show.legend = FALSE) +
   facet_grid(~ treatment)
-  
-ggplot(ref_richness_subset, aes(x = days, y = gene_richness_relative, group = gene, colour = gene)) +
+
+ggplot(sox_richness_subset, aes(x = days, y = gene_richness_relative, group = gene, colour = gene)) +
   geom_line(size = 2) +
   geom_text(aes(label = gene), show.legend = FALSE) +
   facet_grid(~ treatment)
-
+  
+ggplot(ref_richness_subset_glyph, aes(x = days, y = gene_richness_relative, group = gene, colour = gene)) +
+  geom_line(size = 1) +
+  geom_text(aes(label = gene), show.legend = FALSE, size = 3.5) +
+  geom_line(data = phn_operon_richness_subset_glyph, aes(x = days, y = gene_richness_relative, group = gene),size = 0.8)
+  geom_text(data = phn_operon_richness_subset_glyph, label = gene, show.legend = FALSE, size = 3.5)
 
 # generating read abundance data per gene
+
+# reads are not normalized against sequencing depth yet!!!
+
 gene_abundance <- aggregate( . ~  gene + sample + days + treatment, data = gene_reads2, sum)
 gene_abundance <- gene_abundance[,c(1:4, 6)]
 gene_abundance <- gene_abundance[with(gene_abundance, order(gene)), ]
@@ -55,15 +73,19 @@ gene_abundance <- setDT(gene_abundance)
 gene_abundance[,reads_per_gene_relative := reads_per_gene/reads_per_gene[days == 0]*100, by = .(gene,treatment)]
 
 # create subsets
-phn_operon_abundance_subset <- subset(gene_abundance, grepl("phn[C-P]|sox[A-B]", gene))
-ref_abundance_subset <- subset(gene_abundance, !grepl("phn[C-P]|sox[A-B]", gene))
+phn_operon_abundance_subset <- subset(gene_abundance, grepl("phn[C-P]", gene))
+sox_abundance_subset <- subset(gene_abundance, grepl("sox[A-Z]", gene))
+
+# ref_abundance_subset <- subset(gene_abundance, !grepl("phn[C-P]|sox[A-B]", gene))
 
 # plot subsets
-ggplot(phn_operon_abundance_subset, aes(x = days, y = reads_per_gene_relative, group = gene, colour = gene)) +
+ggplot(sox_abundance_subset, aes(x = days, y = reads_per_gene, group = gene, colour = gene)) +
   geom_line(size = 1.5) +
   geom_text(aes(label = gene), show.legend = FALSE) +
   facet_grid(~ treatment)
 
+  
+  
 ggplot(ref_abundance_subset, aes(x = days, y = reads_per_gene_relative, group = gene, colour = gene)) +
   coord_cartesian(ylim = c(0, 400)) +
   geom_line(size = 1.5) +
