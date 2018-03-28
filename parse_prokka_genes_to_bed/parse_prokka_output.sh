f181002c5548c34a57666cdac0c77cafc5a772b2
Johannes Werner <johannes.werner@io-warnemuende.de>
2018-03-28 14:00:58 +0200

    updated latest version, now using product names instead of gene names

--- parse_prokka_genes_to_bed/parse_prokka_output.sh
+++ parse_prokka_genes_to_bed/parse_prokka_output.sh
@@ -7,12 +7,11 @@ usage()
 {
 cat << EOF
 usage:
-$0 -i <INPUT_DIR> -o <OUTPUT_DIR>
+$0 -o <OUTPUT_DIR>
 
 The script creates a bed file based on prokka annotations.
 
 OPTIONS:
-   -i          Input directory (prokka annotation directory, mandatory)
    -o          Output directory (mandatory)
 
    -h/--help   Show this message
@@ -20,11 +19,8 @@ OPTIONS:
 EOF
 }
 
-while getopts ":i:o:" o; do
+while getopts ":o:" o; do
     case "${o}" in
-        i)
-            INPUT_DIR=${OPTARG}
-            ;;
         o)
             OUTPUT_DIR=${OPTARG}
             ;;
@@ -35,9 +31,9 @@ while getopts ":i:o:" o; do
 done
 shift $((OPTIND-1))
 
-if [[ -z $INPUT_DIR ]] && [[ -z $OUTPUT_DIR ]]
+if [[ -z $OUTPUT_DIR ]]
 then
-    echo Parameter -i and or -o is not set. These parameters are required. Exit code: 1. Exiting ...
+    echo Parameter -o is not set. This parameter is required. Exit code: 1. Exiting ...
     echo -en "\n"
     usage
     echo -en "\n"
@@ -62,32 +58,47 @@ fi
 
 cd $BASE_DIR
 
+#for SAMPLE_FOLDER in *
+#do
+#    cd ${SAMPLE_FOLDER}/output_IMP/Analysis/annotation
+##    for GENE_NAME in gyrA gyrB gox phnA phnC phnD phnE phnF phnG phnH phnI phnJ phnK phnL phnM phnN phnX phnW phnR \
+##      phnS phnT phnU phnP pspE pstC purU purB pphA phoR phoB phoP soxA soxB thiO recA rpoC ktrB ydiF nuoF mntB araD \
+##      tfdB artI arfA malF qedA mlhB uvrB fdhA fdm recG
+##     for GENE_NAME in norB nirS nosZ soxR soxS soxC katG* katE czcD merA tmoS chiA* chtA xynB bcsZ celB cenC
+#    for GENE_NAME in dnaJ dnaK dmlR ftsY ftsZ rplB polA gap* sdhA
+#    do
+#        GREP_GENE_NAME="${GENE_NAME}_"
+#        BED_FILENAME=${BED_DIR}/intersect_${GENE_NAME}_${SAMPLE_FOLDER}.bed
+#        grep $GREP_GENE_NAME annotation.filt.gff > ${TMP_DIR}/tmp.gff
+#
+#        cat ${TMP_DIR}/tmp.gff | \
+#          awk -F '\t|;' '{for(i=9;i<=NF;i++){if($i~/^gene=/){column=$i}} print $1,$4,$5,$5-$4,column}' > ${BED_FILENAME}
+#
+#        $SAMTOOLS_BIN view -L ${BED_FILENAME} ../../Assembly/mg.reads.sorted.bam | grep -v -P "^\@" | cut -f 1,3 | \
+#          sort | uniq | cut -f 2  | sort | uniq -c | perl -ane '$_=~/^\s+(\d+) (.+)$/;chomp($2); print "$2\t$1\n"; ' \
+#          > ${RESULTS_DIR}/mg.reads.per.gene_${GENE_NAME}_${SAMPLE_FOLDER}.tsv &
+#
+#        rm ${TMP_DIR}/tmp.gff
+#    done
+#    cd ../../../..
+#done
+
 for SAMPLE_FOLDER in *
 do
     cd ${SAMPLE_FOLDER}/output_IMP/Analysis/annotation
-    for GENE_NAME in gyrA gyrB gox phnA phnC phnD phnE phnF phnG phnH phnI phnJ phnK phnL phnM phnN phnX phnW phnR \
-      phnS phnT phnU phnP pspE pstC purU purB pphA phoR phoB phoP soxA soxB thiO recA rpoC ktrB ydiF nuoF mntB araD \
-      tfdB artI arfA malF qedA mlhB uvrB fdhA fdm recG
+    for PRODUCT_NAME in "Sarcosine oxidase subunit beta" "Sarcosine oxidase%2C gamma subunit family" "Sarcosine oxidase%2C delta subunit family"
     do
-        GREP_GENE_NAME="${GENE_NAME}_"
-        BED_FILENAME=${BED_DIR}/intersect_${GENE_NAME}_${SAMPLE_FOLDER}.bed
-        # get all lines which carry a phn gene and keep only the columns with contig name, start, stop and gene name
-        if [ $GENE_NAME == "pphA" ]
-        then
-            grep $GREP_GENE_NAME annotation.filt.gff | grep "3.11.1.3" > ${TMP_DIR}/tmp.gff
-        elif [ $GENE_NAME == "soxA" ] || [ $GENE_NAME == "soxB" ]
-        then
-            grep $GREP_GENE_NAME annotation.filt.gff | grep "1.5.3.1" > ${TMP_DIR}/tmp.gff
-        else
-            grep $GREP_GENE_NAME annotation.filt.gff > ${TMP_DIR}/tmp.gff
-        fi
+	PRODUCT_NAME_NO_SPACES=`echo ${PRODUCT_NAME} | tr " " "-"`
+        BED_FILENAME=${BED_DIR}/intersect_${PRODUCT_NAME_NO_SPACES}_${SAMPLE_FOLDER}.bed
+        grep "$PRODUCT_NAME" annotation.filt.gff > ${TMP_DIR}/tmp.gff
 
         cat ${TMP_DIR}/tmp.gff | \
-          awk -F '\t|;' '{for(i=9;i<=NF;i++){if($i~/^gene=/){column=$i}} print $1,$4,$5,$5-$4,column}' > ${BED_FILENAME}
+          awk -F '\t|;' '{for(i=9;i<=NF;i++){if($i~/^product=/){column=$i}} print $1,$4,$5,$5-$4,column}' > ${BED_FILENAME}
 
         $SAMTOOLS_BIN view -L ${BED_FILENAME} ../../Assembly/mg.reads.sorted.bam | grep -v -P "^\@" | cut -f 1,3 | \
           sort | uniq | cut -f 2  | sort | uniq -c | perl -ane '$_=~/^\s+(\d+) (.+)$/;chomp($2); print "$2\t$1\n"; ' \
-          > ${RESULTS_DIR}/mg.reads.per.gene_${GENE_NAME}_${SAMPLE_FOLDER}.tsv &
+          > ${RESULTS_DIR}/mg.reads.per.gene_${PRODUCT_NAME_NO_SPACES}_${SAMPLE_FOLDER}.tsv &
+          ### TODO (WARNING): currently all processes are forked at the same time. this needs to be resolved. limit to a certain number of processes.
 
         rm ${TMP_DIR}/tmp.gff
     done
@@ -95,3 +106,4 @@ do
 done
 
 rmdir -p ${OUTPUT_DIR}/tmp
+
