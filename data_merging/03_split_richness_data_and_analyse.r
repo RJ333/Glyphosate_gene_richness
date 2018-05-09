41b6f2b8be0f376ba3c9e42f21dfbdc1ab020a8c
RJ333 <35432752+RJ333@users.noreply.github.com>
2018-05-09 16:12:27 +0200

    #27 categorize gene richness values into groups

---
+++ data_merging/03_split_richness_data_and_analyse.r
@@ -0,0 +1,12 @@
+# in the previous scripts we generated the R object (R workspace "data_merging_glyph_single_assemblies.RData") prokka_all
+# and all_products_richness. Now each gene/product should be assigned to a group, depending on its richness change
+# to determine which group a gene belongs to, certain conditions must be fulfilled
+
+# cell peak value higher than starting value?
+# end value
+# average value?
+
+
+# calculate average richness per product
+
+# check the range from highest to lowest value for all products
