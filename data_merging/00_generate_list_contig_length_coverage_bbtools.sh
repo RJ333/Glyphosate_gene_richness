3a0b410288f94186ef479aaf7ec0a1c3dc2a7e04
RJ333 <35432752+RJ333@users.noreply.github.com>
2018-06-05 10:12:00 +0200

    formatting changes

--- data_merging/00_generate_list_contig_length_coverage_bbtools.sh
+++ data_merging/00_generate_list_contig_length_coverage_bbtools.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
-# add contig length and average coverage of all glyphosate samples to one list and add a column with the sample name  
+# add contig length and average coverage of all glyphosate samples to one list 
+# and add a column with the sample name  
 BASE_DIR=/data/jwerner/glyphosate/IMP/
 OUTPUT_DIR=/data/Rene/glyph/
 
 cd $BASE_DIR
 # removing old versions if existing
-rm ${OUTPUT_DIR}/contig_length_coverage.tsv 
+rm -f ${OUTPUT_DIR}/contig_length_coverage.tsv 
 for contig_coverage in */metagenome_coverage/covstats.txt
 do
   sample=$(echo $contig_coverage |  sed "s/\/.*$//")
   echo "processing $sample"
   # selecting columns with contig_id, average coverage and contig_length 
-  awk -v SAMPLE=$sample 'BEGIN {FS = OFS= "\t"} NR > 1 {print SAMPLE, $1, $2, $3}' $contig_coverage >> ${OUTPUT_DIR}/contig_length_coverage.tsv
+  awk -v SAMPLE=$sample 'BEGIN {FS = OFS= "\t"} 
+    NR > 1 {print SAMPLE, $1, $2, $3}
+	' $contig_coverage >> ${OUTPUT_DIR}/contig_length_coverage.tsv
 done
 echo "output contig_length_coverage.tsv generated in $OUTPUT_DIR"  
