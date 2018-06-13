102680dd03b19122cb9dfc542737eac0f3db60bd
RJ333 <35432752+RJ333@users.noreply.github.com>
2018-06-13 14:10:54 +0200

    #38 as Rscript

--- reads_per_product_samtools/02_modify_prokka_products.r
+++ reads_per_product_samtools/02_modify_prokka_products.r
@@ -31,18 +31,11 @@ args <- parser$parse_args()
 # call like this: 
 # conda activate Renv
 # time Rscript 02_modify_prokka_products.r \
-# -p="/data/Rene/glyph/prokka/prokka_all_modified.tsv" -o="/data/Rene/glyph/prokka/"
-
-
-# read in contig_gene_sample_reads 
-contig_gene_sample_reads <- fread("./omics/prokka/contig_gene_sample_reads.tsv")
-contig_gene_sample_reads$gene <- gsub("_|-|'| |/|:|\\.|\\(|\\)|\\|", "@", contig_gene_sample_reads$gene)  
-
+# -p="/data/Rene/glyph/prokka/prokka_all_modified.tsv" -o="/data/Rene/glyph/prokka/" 
 
 # read in the combined prokka data from all samples 
 print("reading prokka data ...")
-#prokka_tables <- fread(args$prokka_file, 
-prokka_tables <- fread("./omics/prokka/prokka_all_modified.tsv", 
+prokka_tables <- fread(args$prokka_file, 
   col.names = c("sample", "contig_id", "gene_start", "gene_end", "annotation"),
   colClasses = c("factor", "factor", "integer", "integer", "character"))
 
@@ -55,20 +48,16 @@ prokka_neat <- prokka_tables %>%
   filter(!(is.na(Col) & is.na(Value))) %>% 
   spread(Col, Value)
 
-# keep this during devel, if something fails 
-full <- prokka_neat  
-# prokka_neat <- full
-
-
-# combine "note" and "product"
+# combine information from columns "note" and "product"
 print("combining columns product and note")
-prokka_neat$product2 <- ifelse(prokka_neat$product == "hypothetical protein" & !is.na(prokka_neat$note), prokka_neat$note, prokka_neat$product)
+prokka_neat$product2 <- ifelse(prokka_neat$product == "hypothetical protein" & !is.na(prokka_neat$note), 
+  prokka_neat$note, prokka_neat$product)
 
 # calculate gene length
 print("calculating gene length")
 prokka_neat$gene_length <- prokka_neat$gene_end - prokka_neat$gene_start
 
-# remove unneeded columns "note" and "product"
+# remove now unneeded columns "note" and "product"
 # gene start and end is needed for bed file generation!
 print("selecting important columns")
 prokka_select <- prokka_neat[, c("sample", "contig_id", "gene_start", "gene_end", "gene_length",
@@ -79,8 +68,7 @@ print("unifying product annotations")
 prokka_select$product2 <- tolower(prokka_select$product2)
 
 ## replace white lines and special characters in product names with "@"
-prokka_select$product2 <- gsub("_|-|'| |/|:|\\(|\\.|\\)|\\|", "@", prokka_select$product2)  
-
+prokka_select$product2 <- gsub("_|-|'| |/|:|\\(|\\.|\\)|\\|", "@", prokka_select$product2)
 
 # unify gene numbering e.g. genE_01, genE_02 etc --> genE
 print("unifying gene names")
@@ -100,29 +88,15 @@ unique_products_selected <- all_unique_products_with_count %>%
 filter(Freq > args$threshold) %>%
 select(Var1)
 
-unique_products_selected <- all_unique_products_with_count %>%
-filter(Freq > 1) %>%
-select(Var1)
-
-
 # unique products for samtools script
 print("writing unique products across all samples ...")
 write.table(unique_products_selected , sep = "\t", 
   file = paste0(args$output_dir, "/unified_unique_prokka_products_greater_",args$threshold,".tsv"))
 print("all done")
 
-# unique products for samtools script
-print("writing unique products across all samples ...")
-write.table(unique_products_selected , sep = "\t", 
-  file = paste0("./omics/prokka", "/unified_unique_prokka_products_greater_",1,".tsv"))
-print("all done")
-
-
 # generate prokka output for samtools to create bed.files
 prokka_bed  <- setDT(prokka_select)
 prokka_bed[, fwrite(.SD, sep = "\t", paste0(args$output_dir, "/prokka_for_bed_", sample,".tsv")), by = sample]
-
-prokka_bed[, fwrite(.SD, sep = "\t", paste0("./omics/prokka", "/prokka_for_bed_", sample,".tsv")), by = sample]
 
 # remove gene start/end from this file (column 3 and 4)
 prokka_processed <- prokka_select[, c("sample", "contig_id", "gene_length",
@@ -131,7 +105,6 @@ prokka_processed <- prokka_select[, c("sample", "contig_id", "gene_length",
 # write prokka files for further merging
 print("writing processed prokka file ...")
 write.table(prokka_processed , sep = "\t", file = paste0(args$output_dir, "/prokka_processed.tsv"))
-write.table(prokka_processed , sep = "\t", file = paste0("./omics/prokka", "/prokka_processed.tsv"))
 print("all done")
 
 sessionInfo()
