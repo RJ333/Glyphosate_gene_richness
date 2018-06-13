3ae8390d64b53aa5def284bbd0136b29183676bc
RJ333 <35432752+RJ333@users.noreply.github.com>
2018-06-13 14:38:53 +0200

    #38 reduce samtools step

--- reads_per_product_samtools/04_get_reads_per_prokka_product_GNU_parallel.sh
+++ reads_per_product_samtools/04_get_reads_per_prokka_product_GNU_parallel.sh
@@ -11,18 +11,16 @@
 # ctrl+b, n # next
 # ctrl+b, p # previous
 
-# how often needs a product be present per sample
-THRESHOLD=1
 # how many cores should GNU Parallel use for samtools?
 GNU_CORES=44
 
 # directories of original data
 ORIGINAL_BASE_DIR=/data/jwerner/glyphosate/IMP/
-ORIGINAL_PROKKA_DIR=/data/jwerner/glyphosate/IMP/${sample}/output_IMP/Analysis/annotation/ 
+ORIGINAL_PROKKA_DIR=  #/data/jwerner/glyphosate/IMP/${sample}/output_IMP/Analysis/annotation/ 
 SAMTOOLS_BIN=/data/jwerner/tools/samtools-1.7/samtools
 
 # working directory
-OUTPUT_DIR=/data/Rene/glyph/prokka  
+OUTPUT_DIR=  # /data/Rene/glyph/prokka  
 
 # results directories
 BED_DIR=${OUTPUT_DIR}/bed
@@ -32,48 +30,15 @@ TMP_DIR=${OUTPUT_DIR}/tmp
 mkdir -p $BED_DIR
 mkdir -p $RESULTS_DIR
 
+
+## TODO:
+
 ## include sample vector?
 ## include unique list argument?
-
-
-# get all product names from annotation in lower case and combine them with the original prokka file:
-cd $OUTPUT_DIR
-rm $OUTPUT_DIR/unified_all_samples.tsv
-for sample in $(find . -mindepth 1 -maxdepth 1 -type d | sed "s|^\./||")
-do
-  for prokka_annot in ${OUTPUT_DIR}/${sample}/annotation.filt.gff
-  do
-    awk -v SAMPLE=$sample \
-	  '\
-	  BEGIN { FS = "\t|;"; OFS = "\t"}\
-	  {match($0,/product=[^;]*/); product_value=substr($0,RSTART,RLENGTH); {print tolower(product_value), SAMPLE}}' $prokka_annot | tr '/()[:blank:]-_' '@@@@@@'  > $OUTPUT_DIR/unified_${sample}.tsv
-	  # pasting the modified products column to the original prokka file
-	  paste -d '\t' $prokka_annot $OUTPUT_DIR/unified_${sample}.tsv > $OUTPUT_DIR/${sample}/${sample}_prokka_modified.tsv
-	  cat $OUTPUT_DIR/unified_${sample}.tsv >> $OUTPUT_DIR/unified_all_samples.tsv
-	  # remove the for-pasting tables
-	  rm $OUTPUT_DIR/unified_${sample}.tsv
-	  rm $prokka_annot
-  done
-done
-
-# generate a combined prokka modified output for all samples
-cd $OUTPUT_DIR
-for prokka_table in *_prokka_modified.tsv
-do
-  sample=$(echo $prokka_table | sed "s/_.*$//")
-  awk -v sample=$sample '{a = sample;} {print a"\t"$0}' $prokka_table >> $OUTPUT_DIR/all_prokka_modified.gff
-  echo "$sample appended"
-done
-cat $OUTPUT_DIR/all_prokka_modified.gff | rev | sed 's/^@//' | rev > $OUTPUT_DIR/all_prokka_modified2.gff
-rm $OUTPUT_DIR/all_prokka_modified.gff
-
-# use the list of products from all samples as list of queries for samtools. remove duplicates and set minimum threshold
-cd $OUTPUT_DIR
-sort unified_all_samples.tsv| rev | sed 's/@/\t/' | rev | cut -f 1 |\
-sort| uniq -c| awk -v THRESHOLD=$THRESHOLD '$1 > THRESHOLD {print $0}' |\
-awk '{split($2,a,"="); print a[2]}' > unified_unique_prokka_products_greater_${THRESHOLD}.tsv
-
 ## sam_parallel shouldn't use original prokka tables anymore --> R modified ones
+# check if all variables are needed
+# which columns are the needed ones?
+
 sam_parallel() {
 	SAMPLE="$1"
 	cd $OUTPUT_DIR
