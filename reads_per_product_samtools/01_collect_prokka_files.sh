e432b19af44fe8ded026e60132f26b988c245210
RJ333 <35432752+RJ333@users.noreply.github.com>
2018-06-14 16:43:25 +0200

    #38 modified path

--- reads_per_product_samtools/01_collect_prokka_files.sh
+++ reads_per_product_samtools/01_collect_prokka_files.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-OUTPUT_DIR=/data/Rene
+OUTPUT_DIR=/data/Rene/glyph/prokka
 ORIGINAL_BASE_DIR=/data/jwerner/glyphosate/IMP
 PROKKA_PATH=output_IMP/Analysis/annotation
 
@@ -9,7 +9,7 @@ PROKKA_PATH=output_IMP/Analysis/annotation
 # files from all samples, extracts the important columns and adds them to prokka_all_modified
 # including the sample name as first column
 # the output can then be used as input for R with splitstackshape 
-
+mkdir -p $OUTPUT_DIR
 rm -f $OUTPUT_DIR/prokka_all_modified.tsv 
 
 for sample in A1 A2 A3 A4 A5 A6 A7 B8 B9 B10
