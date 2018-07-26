9d868749b87481b32f87dc3b451ca8c7be5c70d4
RJ333 <35432752+RJ333@users.noreply.github.com>
2018-07-26 17:06:24 +0200

    update on interactive analyis, ribosomal proteins as single copy genes

--- single_assembly_gene_richness_and_reads_mapping/0x_plotting_of_merged_data.r
+++ single_assembly_gene_richness_and_reads_mapping/0x_plotting_of_merged_data.r
@@ -4,7 +4,8 @@
 # data is prepared in 03_calculate_and_plot_gene_richness.R
 #and 01_funct_tax_data_merging.bash
 
-
+library(ggplot2)
+library(dplyr)
 # plot taxonomic richness
 
 ggplot(tax_rich_omics, aes(x = days, colour = treatment))+
@@ -15,7 +16,6 @@ ggplot(tax_rich_omics, aes(x = days, colour = treatment))+
   
   
 # plot gene richness
-
 sarc <- subset(gene_richness2, grepl("arcosin", product2))
 sarc <- subset(sarc, grepl("oxidase", product2))
 sarc <- subset(sarc, grepl("delta", product2))
@@ -35,10 +35,7 @@ ggplot(sarc, aes(colour = product2))+
   geom_line(data = tax_rich_omics, aes(x = days, y = richness_rel, colour = "black"), linetype = 2) + 
   facet_wrap(~treatment, nrow = 2) 
 
-# relative abundance and relative richness are not correlated?? (phnC, phnD, phnH
-# more correlation for phnE,
-# differences in reaction time? because of detection limit per contig?
-# back to starting niveau for abundance? or richness? elevated richness over time?
+
 phn_op <- subset(prokka_all, treatment == "glyph" & grepl("phn[C-N]", gene))
 sox_all <- subset(prokka_all, treatment == "glyph" & grepl("sarcosine@oxidase", product2))
 
@@ -55,14 +52,8 @@ sum(head(test, 4600))
 phn_op_rich <- subset(gene_richness2, treatment == "glyph" & grepl("phn[C-N]", gene))
 sox_all_rich <- subset(gene_richness2, treatment == "glyph" & grepl("sarcosine@oxidase", product2))
 
-# gallaeci besitzt soxBCD gene auf vier contigs und phnCDE 1 contig H noch ein contig
 
-# methylo shikimate dehydrogenase? aroE, 1.1.1.25
 
-#Pseudomonas hat phn[C-N] und alle sox gene
-#Hoeflea soxBCD, alle phn außer JK
-
-#plots aller relevanten OTUS
 #Anteil von phn Genen und sox Genen ohne taxonomische Zuordnung?
 table(is.na(phn_op$genus))
 FALSE  TRUE 
@@ -72,11 +63,89 @@ table(is.na(sox_all$genus))
 FALSE  TRUE 
    71   415 
 
-#taxonomie sollte nicht überbewerttet werden
+
+#------------------------------------------------------------------ assessment of single copy and housekeeping genes
+
+   
+#what genes do we have from Wu et al marker genes?
+levels(droplevels(subset(gene_richness2, grepl("rpl", gene))$gene))
+levels(droplevels(subset(gene_richness2, grepl("rps", gene))$gene))   
+
+table(droplevels(subset(gene_richness2, grepl("rps", gene))$gene))
+table(droplevels(subset(gene_richness2, grepl("rpl", gene))$gene))
+table(droplevels(subset(prokka_all, grepl("rpl", gene))$gene))
+
+
+#translation table product2 gene
+gene_product2 <- unique(gene_richness2[, c(1, 2)])
+
+rpl_A1 <- as.data.frame(table(droplevels(subset(prokka_all, grepl("ribosomal@protein@l", product2) & sample == "A1")$product2)))
+rpl_A2 <- as.data.frame(table(droplevels(subset(prokka_all, grepl("ribosomal@protein@l", product2) & sample == "A2")$product2)))
+rpl_A3 <- as.data.frame(table(droplevels(subset(prokka_all, grepl("ribosomal@protein@l", product2) & sample == "A3")$product2)))
+rpl_A4 <- as.data.frame(table(droplevels(subset(prokka_all, grepl("ribosomal@protein@l", product2) & sample == "A4")$product2)))
+rpl_A5 <- as.data.frame(table(droplevels(subset(prokka_all, grepl("ribosomal@protein@l", product2) & sample == "A5")$product2)))
+rpl_A6 <- as.data.frame(table(droplevels(subset(prokka_all, grepl("ribosomal@protein@l", product2) & sample == "A6")$product2)))
+rpl_A7 <- as.data.frame(table(droplevels(subset(prokka_all, grepl("ribosomal@protein@l", product2) & sample == "A7")$product2)))
+rpl_B8 <- as.data.frame(table(droplevels(subset(prokka_all, grepl("ribosomal@protein@l", product2) & sample == "B8")$product2)))
+rpl_B9 <- as.data.frame(table(droplevels(subset(prokka_all, grepl("ribosomal@protein@l", product2) & sample == "B9")$product2)))
+rpl_B10 <- as.data.frame(table(droplevels(subset(prokka_all, grepl("ribosomal@protein@l", product2) & sample == "B10")$product2)))
+
+rpl <- Reduce(function(x,y) merge(x = x, y = y, by = "Var1"), 
+  list(rpl_A1, rpl_A2, rpl_A3, rpl_A4, rpl_A5, rpl_A6, rpl_A7, rpl_B8, rpl_B9, rpl_B10))
+names(rpl) <- c("product2", "A1", "A2", "A3", "A4", "A5", "A6", "A7", "B8", "B9", "B10")
+rpl <- merge(rpl, gene_product2, by = "product2")
+rpl[with(rpl, order(rpl$product2, as.character(rpl$gene), decreasing = TRUE)),]
+rpl %>% arrange(product2, desc(gene)) %>% distinct(product2, A1, A2, A3, A4, A5, .keep_all = TRUE)
+
+
+rps_A1 <- as.data.frame(table(droplevels(subset(prokka_all, grepl("ribosomal@protein@s", product2) & sample == "A1")$product2)))
+rps_A2 <- as.data.frame(table(droplevels(subset(prokka_all, grepl("ribosomal@protein@s", product2) & sample == "A2")$product2)))
+rps_A3 <- as.data.frame(table(droplevels(subset(prokka_all, grepl("ribosomal@protein@s", product2) & sample == "A3")$product2)))
+rps_A4 <- as.data.frame(table(droplevels(subset(prokka_all, grepl("ribosomal@protein@s", product2) & sample == "A4")$product2)))
+rps_A5 <- as.data.frame(table(droplevels(subset(prokka_all, grepl("ribosomal@protein@s", product2) & sample == "A5")$product2)))
+rps_A6 <- as.data.frame(table(droplevels(subset(prokka_all, grepl("ribosomal@protein@s", product2) & sample == "A6")$product2)))
+rps_A7 <- as.data.frame(table(droplevels(subset(prokka_all, grepl("ribosomal@protein@s", product2) & sample == "A7")$product2)))
+rps_B8 <- as.data.frame(table(droplevels(subset(prokka_all, grepl("ribosomal@protein@s", product2) & sample == "B8")$product2)))
+rps_B9 <- as.data.frame(table(droplevels(subset(prokka_all, grepl("ribosomal@protein@s", product2) & sample == "B9")$product2)))
+rps_B10 <- as.data.frame(table(droplevels(subset(prokka_all, grepl("ribosomal@protein@s", product2) & sample == "B10")$product2)))
+
+rps <- Reduce(function(x,y) merge(x = x, y = y, by = "Var1"), 
+  list(rps_A1, rps_A2, rps_A3, rps_A4, rps_A5, rps_A6, rps_A7, rps_B8, rps_B9, rps_B10))
+names(rps) <- c("product2", "A1", "A2", "A3", "A4", "A5", "A6", "A7", "B8", "B9", "B10")
+rps <- merge(rps, gene_product2, by = "product2")
+rps[with(rps, order(rps$product2, as.character(rps$gene), decreasing = TRUE)),]
+rps %>% arrange(product2, desc(gene)) %>% distinct(product2, A1, A2, A3, A4, A5, .keep_all = TRUE)
+
+#-----------------------------------------------------------------------
+
+
+
+#fragen?
 
 #ist richness empfindlicher als abundanz? wie teste ich das im vergleich
 #zu "normalen" Genen?
-   
+
+# relative abundance and relative richness are not correlated?? (phnC, phnD, phnH
+# more correlation for phnE,
+# there is an excel table on that findings "richness_abundance comparison"
+# differences in reaction time? because of detection limit per contig?
+# back to starting niveau for abundance? or richness? elevated richness over time?
+
+# To do
+#plots aller relevanten OTUS
+
+# taxonomy + gene:
+
+# gallaeci besitzt soxBCD gene auf vier contigs und phnCDE 1 contig H noch ein contig
+# methylo shikimate dehydrogenase? aroE, 1.1.1.25
+#Pseudomonas hat phn[C-N] und alle sox gene
+#Hoeflea soxBCD, alle phn außer JK
+#taxonomie sollte nicht überbewerttet werden
+
+
+
+
+
 as.data.frame(table(droplevels(phn_op$genus)))
 
 as.data.frame(table(droplevels(sox_op$genus)))
