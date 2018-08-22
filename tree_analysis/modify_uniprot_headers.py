94a28ccc68e1c42839d8b6ab8b54e24c0b06d167
RJ333 <35432752+RJ333@users.noreply.github.com>
2018-08-22 15:16:57 +0200

    #49 adjusted regex for uniprot headers

--- tree_analysis/modify_uniprot_headers.py
+++ tree_analysis/modify_uniprot_headers.py
@@ -9,7 +9,7 @@ file = sys.argv[1]
 with open(file) as f:
     for line in f:
         line_strip = line.rstrip()
-        rx_object = re.search(r"^(>tr[A-Z0-9\|_]+) .+( OS=.+) OX=", line_strip)
+        rx_object = re.search(r"^(>[a-z]+[A-Z0-9\|_]+) .+( OS=.+) OX=", line_strip)
         if rx_object:
             new_header = rx_object.group(1) + rx_object.group(2)
             print(new_header.replace(" ", "@"))
