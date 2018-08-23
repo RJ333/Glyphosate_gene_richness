4bc61e96b7396b9e7243ccfd900f33ecedd5aa93
RJ333 <35432752+RJ333@users.noreply.github.com>
2018-08-23 16:11:46 +0200

    #49 added sample array

--- tree_analysis/general_setup_for_tree_analysis.sh
+++ tree_analysis/general_setup_for_tree_analysis.sh
@@ -4,25 +4,27 @@
 
 # directories of original data
 BASE_DIR=/data/Rene/glyph/tree/
-REF_DIR=${BASE_DIR}/uniprot_sequences
 GIT_REPO=/data/Rene/Glyphosate_gene_richness/tree_analysis
-SUB_DIR=${BASE_DIR}/trees_degradation
 PROKKA_DIR=/data/jwerner/glyphosate/IMP/${sample}/output_IMP/Analysis/annotation/
+
+SAMPLE_ARRAY=(A1 A2 A3 A4 A5 A6 A7 B8 B9 B10)
 
 mkdir -p ${BASE_DIR}/prokka_original/
 mkdir -p ${BASE_DIR}/prokka_modified/
 
-# copy own data into tree working folder
-for SAMPLE in A1 A2 A3 A4 A5 A6 A7 B8 B9 B10
+# copy own data into tree base folder
+for SAMPLE in "${SAMPLE_ARRAY[@]}"
 do 
     echo $SAMPLE
 	cp ${PROKKA_DIR}/prokka.faa ${BASE_DIR}/prokka_original/${SAMPLE}_prokka.faa
 done
 
+echo "prokka files have been copied"
+
 # original Prokka data was put into a single line, 
 # sample names were included in header and whitespaces replaced with @
 
-for SAMPLE in A1 A2 A3 A4 A5 A6 A7 B8 B9 B10
+for SAMPLE in "${SAMPLE_ARRAY[@]}"
 do 
     echo $SAMPLE
 	awk '{if (!/>/) {printf "%s",$0;next} else {printf "%s%s%s","\n",$0,"\n"}}' \
@@ -31,3 +33,5 @@ do
 	  sed "s/ /_${SAMPLE} /" | \
 	  sed "s/ /@/g" > prokka_modified/${SAMPLE}_prokka.oneline.sampleheader.nospaces.faa
 done
+
+echo "prokka files have been modified"
--- tree_analysis/modify_uniprot_headers.py
+++ tree_analysis/modify_uniprot_headers.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+# use python2
+
 import re
 import sys
 
