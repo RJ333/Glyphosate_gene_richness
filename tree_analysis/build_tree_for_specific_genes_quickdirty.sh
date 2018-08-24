89255582af56332ac43ec3f0d62c44f9fde638cc
janssen <rene.janssen@io-warnemuende.de>
2018-08-24 10:28:20 +0200

    #49 adjusted to look for product instead gene

--- tree_analysis/build_tree_for_specific_genes_quickdirty.sh
+++ tree_analysis/build_tree_for_specific_genes_quickdirty.sh
@@ -1,38 +1,38 @@
 cd /data/Rene/glyph/trees_degradation/msa
-mkdir phnH
-cd phnH
-cp  /data/Rene/glyph/trees_degradation/uniprot/sequences_modified/*phnH* .
+mkdir phnJ
+cd phnJ
+cp  /data/Rene/glyph/trees_degradation/uniprot/sequences_modified/*phnJ* .
 
 
 # take all sequences from reference data
-cat *.fasta >> all.phnH.reference.fasta
+cat *.fasta >> all.phnJ.reference.fasta
 
 
-# take all phnH sequences from own data
+# take all phnJ sequences from own data
 egrep -h -A 1 "^>" /data/Rene/glyph/tree/prokka_modified/* |\
-  egrep -A 1 "PhnH" | sed '/^--$/d' > own.phnH.faa
+  egrep -i -A 1 "phosphate@c-p@lyase" | sed '/^--$/d' > own.phnJ.faa
 
 # perform cd-hit for reference data to reduce amount of sequences for tree
-cd-hit -i all.phnH.reference.fasta -o phnH.ref.cdhit75.faa -c 1 -d 0
+cd-hit -i all.phnJ.reference.fasta -o phnJ.ref.cdhit75.faa -c 1 -d 0
 
 # removing duplicates (own data)
-cd-hit-dup -i own.phnH.faa -o phnH.own.cdhit100.faa
+cd-hit-dup -i own.phnJ.faa -o phnJ.own.cdhit100.faa
 
 # merge two one file with all sequences for alignment and tree
-cat phnH.own.cdhit100.faa > phnH.all.faa
-cat phnH.ref.cdhit75.faa >> phnH.all.faa
+cat phnJ.own.cdhit100.faa > phnJ.all.faa
+cat phnJ.ref.cdhit75.faa >> phnJ.all.faa
 
 # multipe sequence alignment
-mafft --auto phnH.all.faa > phnH.all.msa.faa
+mafft --auto phnJ.all.faa > phnJ.all.msa.faa
 
 #convert to phylip
 /data/Rene/glyph/trees_degradation/tree/fasta_to_phylip.py \
-phnH.all.msa.faa phnH.all.msa.faa.phy
+phnJ.all.msa.faa phnJ.all.msa.faa.phy
 
 # remove unwanted characters
-python2 /data/Rene/glyph/trees_degradation/tree/remove_special_characters.py phnH.all.msa.faa.phy \
-  > phnH.all.msa.faa.clean.phy
+python2 /data/Rene/glyph/trees_degradation/tree/remove_special_characters.py phnJ.all.msa.faa.phy \
+  > phnJ.all.msa.faa.clean.phy
   
-cat phnH.all.msa.faa.clean.phy > phnH.all.cdhit95.msa.faa.clean.phy
+cat phnJ.all.msa.faa.clean.phy > phnJ.all.cdhit100.msa.faa.clean.phy
   
   
