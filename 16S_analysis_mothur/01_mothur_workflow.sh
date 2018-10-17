7e0750e71cbc03e8cb23ae48d57d3681021f678c
RJ333 <35432752+RJ333@users.noreply.github.com>
2018-10-17 17:23:37 +0200

    #51 running make.contigs()

--- 16S_analysis_mothur/01_mothur_workflow.sh
+++ 16S_analysis_mothur/01_mothur_workflow.sh
@@ -1,7 +1,7 @@
 # getting data and folders set up
 
-mkdir -p /data/projects/glyphosate/reads/raw_reads
-
+mkdir -p /data/projects/glyphosate/reads/raw_reads_16S
+mkdir -p /data/projects/glyphosate/reads/processed
 # create soft links to gzipped read files
 
 ln -s /data/Rene/miseq/biofilm_dna_cdna_600/fastq/*.gz /data/projects/glyphosate/reads/raw_reads
@@ -22,6 +22,38 @@ ln -s /data/Rene/miseq/lars_janine/fastq/R*/*.gz /data/projects/glyphosate/reads
 
 zcat R12_S96_L001_R1_001.fastq.gz | head
 
-# remove undetermined reads
+# remove undetermined reads and sample Mo7
 
 rm *determined*.gz
+rm Mo7*.gz
+
+for file in c-*; do mv "$file" `echo $file | tr '-' '_'` ; done
+
+# current silva database
+mv /data/Rene/SILVA_132_SSURef_NR99_13_12_17_opt.arb.gz /data/db/silva
+
+# unzip silva database ??
+
+
+# to start mothur in conda, add the following lines to bashrc on bio-49 and save
+vi ~/.bashrc
+
+# . /data/tools/miniconda3/etc/profile.d/conda.sh
+# conda activate
+
+source ~/.bashrc
+
+# start mothur environment
+conda activate mothur
+
+mothur
+
+# set output dir
+set.dir(output=/data/projects/glyphosate/reads/processed)
+
+# make stability file
+# Group names should not include :, -, or / characters
+make.file(inputdir=/data/projects/glyphosate/reads/raw_reads_16S, type = gz)
+
+make.contigs(file=stability.files, processors=45)	
+
