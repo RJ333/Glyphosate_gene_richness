d4b774d9a052369d24eada425316c62f29a60715
janssen <rene.janssen@io-warnemuende.de>
2018-10-29 00:27:08 +0100

    #52 #54 further adjusting dada2 workflow

--- 16S_analysis_dada2/00_install_dada2_conda.sh
+++ 16S_analysis_dada2/00_install_dada2_conda.sh
@@ -1,24 +1,49 @@
 # download silva training set from 
 # https://www.dropbox.com/sh/mfcivbudmc21cqt/AAB1l-AUM5uKvjrR33ct-cTXa?dl=0
 
-# using -P, this somehow was downloaded to dada2_tutorial/, but not into db?
-wget -O /data/projects/dada2_tutorial/db/silva_nr_v123_train_set.fa.gz\
-  'https://www.dropbox.com/sh/mfcivbudmc21cqt/AAAPdfcBQgLSS5JmnjCDMuWfa/silva_nr_v123_train_set.fa.gz?dl=0'
-cd /data/projects/dada2_tutorial/
-mv silva_nr_v123_train_set.fa.gz ./db
+# https://rdrr.io/bioc/dada2/man/makeSpeciesFasta_Silva.html
+# https://zenodo.org/record/1172783 prepared silva 132 training and species assignment
 
-wget -O /data/projects/dada2_tutorial/db/silva_species_assignment_v123.fa.gz\ 
-  'https://www.dropbox.com/sh/mfcivbudmc21cqt/AAAvMTycF0dhRWJJt_CJLcbia/silva_species_assignment_v123.fa.gz?dl=0'
 
+# download the silva training set and the silva species assignment
+# with the links above, those can also be created
+
+mkdir -p /data/db
+
+wget -O /data/db/silva_nr_v132_train_set.fa.gz\
+  'https://zenodo.org/record/1172783/files/silva_nr_v132_train_set.fa.gz?download=1'
+wget -O /data/db/silva_species_assignment_v132.fa.gz\
+  'https://zenodo.org/record/1172783/files/silva_species_assignment_v132.fa.gz?download=1'
+ 
+mkdir -p /data/projects/glyphosate/analysis/metadata
+mkdir .p /data/projects/glyphosate/analysis/dada2
+scp -i ~/.ssh/denbi.key /mnt/d/denbi/meta_dna_water.csv centos@193.196.20.111:/data/projects/glyphosate/analysis/metadata/
+
+ 
 # install Bioconductor and dada2 in conda dada2
 conda config --add channels defaults
 conda config --add channels bioconda
 conda config --add channels conda-forge
 
 conda create -n dada2 bioconductor-shortread=1.38.0 r-base=3.5.1 bioconductor-dada2=1.8
-
-# if conda is not available, e.g. on io-49
-source ~/.bashrc
-
 conda activate dada2
+conda install cutadapt=1.18
+sudo yum install pigz  # allows parallel gzip
+
+
+cd /data/projects/glyphosate/reads/raw_reads_16S/water_dna
+
+vim cutadapt_dna_water.sh
+#!/bin/bash
+# this script assumes that f and r reads are still separated
+# and primer set 341f-805r (V3-V4)
+
+input=$1 # argument passing has not been tested yet
+output=$2
+
+for xy in $1/*R*.gz
+do
+  cutadapt -j 20 -g CCTACGGGNGGCWGCAG -g GACTACHVGGGTATCTAATCC -o $2/cut_${xy} ${xy} 
+done
+
 R
--- 16S_analysis_dada2/01_modify_dada2_input_for_own_Data.r
+++ 16S_analysis_dada2/01_modify_dada2_input_for_own_Data.r
@@ -2,15 +2,9 @@
 
 # check meta data csv
 
-/mnt/d/data/mothur_sample
-
 # define working directory to story RData image
-# setwd("/mnt/d/data/mothur_sample")
-setwd("D:/data/mothur_sample")
-
-wget -O /mnt/d/data/mothur_sample/metadata/metadata.csv\
-  'https://www.dropbox.com/s/wpxnvsui4mjj8z3/MIMARKS_Data_combined.csv?raw=1'
-
+setwd("/data/projects/glyphosate/reads/dada2_processed/water_dna")
+load(file = "dada2_water_dna.RData")
 ### the outcommented steps are only required the first time
 
 # first installing these packages manually
@@ -39,49 +33,137 @@ sapply(c(.cran_packages, .bioc_packages), require, character.only = TRUE)
 
 set.seed(100)
 
+miseq_path <- file.path("/data/projects/glyphosate/reads/reads_16S_cutadapt", "water_dna")
+filt_path <- file.path("/data/projects/glyphosate/reads/dada2_processed", "water_dna")
+
+
 # here starts the processing
-fns <- sort(list.files("D:/data/mothur_sample", full.names = TRUE))
+fns <- sort(list.files(miseq_path, full.names = TRUE))
 fnFs <- fns[grepl("R1", fns)]
 fnRs <- fns[grepl("R2", fns)]
 
 # Trim and Filter
-
-# takes 3 random samples and plots their quality value over length
 ii <- sample(length(fnFs), 3)
 for(i in ii) { print(plotQualityProfile(fnFs[i]) + ggtitle("Fwd")) }
 for(i in ii) { print(plotQualityProfile(fnRs[i]) + ggtitle("Rev")) }
 
-# generates file names for filtered files
-if(!file_test("-d", "D:/data/mothur_sample/filtered")) dir.create("D:/data/mothur_sample/filtered")
-filtFs <- file.path("D:/data/mothur_sample/filtered", basename(fnFs))			# be careful, the original script 
-filtRs <- file.path("D:/data/mothur_sample/filtered", basename(fnRs))			# contains filtFs and filt"s"Fs!!
-# trimming reads
+# forward reads 10 to 280
+# reverse reads 10 to 240
+
+if(!file_test("-d", filt_path)) dir.create(filt_path)
+filtFs <- file.path(filt_path, basename(fnFs))			# be careful, the original script 
+filtRs <- file.path(filt_path, basename(fnRs))			# contains filtFs and filt"s"Fs!!
 for(i in seq_along(fnFs)) {
   fastqPairedFilter(c(fnFs[[i]], fnRs[[i]]),
 		      c(filtFs[[i]], filtRs[[i]]),
-                      trimLeft = 10, truncLen = c(245, 160),
+                      trimLeft = 10, truncLen = c(280, 240),
                       maxN = 0, maxEE = 2, truncQ = 2,
                       compress = TRUE)
 }
 
+# Trim and Filter
+ii <- sample(length(fnFs), 3)
+for(i in ii) { print(plotQualityProfile(filtFs[i]) + ggtitle("Fwd filt")) }
+for(i in ii) { print(plotQualityProfile(filtRs[i]) + ggtitle("Rev filt")) }
+
+
 # Infer sequence variants
 derepFs <- derepFastq(filtFs)
 derepRs <- derepFastq(filtRs)
-sam.names <- sapply(strsplit(basename(filtFs), "_"), `[`, 1)
+sam.names <- sapply(strsplit(basename(filtFs), "_S"), `[`, 1) # split character adjusted
 names(derepFs) <- sam.names
 names(derepRs) <- sam.names
 
+# adjusted to 50 samples for training
+ddF <- dada(derepFs[1:50], err = NULL, 
+			selfConsist = TRUE) # Convergence after  6  rounds. about 2 hours?
 
-ddF <- dada(derepFs[1:40], err = NULL, 
-			selfConsist = TRUE) # Convergence after 8 rounds.> 6 hours, bio-48???? 
-								# few minutes on laptop
-ddR <- dada(derepRs[1:40], err = NULL, 
-			selfConsist = TRUE) # Convergence after 7 rounds.> 1 hour, bio-49
-								# few minutes on Laptop
+			
+ddR <- dada(derepRs[1:50], err = NULL, 
+			selfConsist = TRUE) # Convergence after 
 
-plotErrors(ddF) # this doesn't work yet due to failing x11 forwarding (ubuntu subsystem, bio 49?)
-plotErrors(ddR)	# but x11 forwarding works with mobaxterm	
+# plotErrors(ddF)
+# plotErrors(ddR)	
 
-test <- dada(derepFs, err = ddF[[1]]$err_out, pool = TRUE) # takes about 2 - 3 hours without multithread
-dadaRs <- dada(derepRs, err = ddR[[1]]$err_out, pool = TRUE, multithread = TRUE) # 20 mins for multiple cores
+dadaFs <- dada(derepFs, err = ddF[[1]]$err_out, pool = TRUE, multithread = TRUE) # for multiple cores
+dadaRs <- dada(derepRs, err = ddR[[1]]$err_out, pool = TRUE, multithread = TRUE)
 mergers <- mergePairs(dadaFs, derepFs, dadaRs, derepRs)
+
+# Construct sequence table and remove chimeras
+seqtab.all <- makeSequenceTable(mergers)
+seqtab <- removeBimeraDenovo(seqtab.all)
+
+# include deseq2 here?
+# we could probably combine the different sequencing runs here before assigning taxonomy
+
+# Assign taxonomy (I used silva instead of rdp classifier)
+# train set and species assignment can also be created from the original silva release file
+# path <- "data/db/Silva.nr_v132"
+# dada2:::makeTaxonomyFasta_Silva(file.path(path, "silva.nr_v132.align"), file.path(path, "silva.nr_v132.tax"), "data/db/silva_nr_v132_train_set.fa.gz")
+# dada2:::makeSpeciesFasta_Silva("data/db/SILVA_132_SSURef_tax_silva.fasta.gz", "~/tax/silva_species_assignment_v132.fa.gz")
+
+ref_fasta <- "/data/db/silva_nr_v132_train_set.fa.gz"
+taxtab <- assignTaxonomy(seqtab, refFasta = ref_fasta)
+colnames(taxtab) <- c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus")
+seqs <- getSequences(seqtab)
+names(seqs) <- seqs # This propagates to the tip labels of the tree
+alignment <- AlignSeqs(DNAStringSet(seqs), anchor = NA)
+
+# phangorn package: we first construct a neighbor-joining tree,
+phang.align <- phyDat(as(alignment, "matrix"), type = "DNA")
+dm <- dist.ml(phang.align)
+treeNJ <- NJ(dm) # Note, tip order != sequence order
+fit = pml(treeNJ, data = phang.align)
+
+# then fit a GTR+G+I maximum likelihood tree using the neighbor-joining tree as a starting point.
+fitGTR <- update(fit, k = 4, inv = 0.2)
+# the following step takes longer
+fitGTR <- optim.pml(fitGTR, model = "GTR", optInv = TRUE, optGamma = TRUE,
+                      rearrangement = "stochastic", control = pml.control(trace = 0))
+detach("package:phangorn", unload=TRUE)
+
+save.image(file = "dada2_water_dna.RData")
+
+# don't forget the controls in meta data? 
+# check MIMARKS example
+
+# Combine data into a phyloseq object (csv download link only displayed in V1 of the protocol)					  
+# mimarks_path <- file.path(tempdir(), "MIMARKS.csv")
+# download.file("https://www.dropbox.com/s/wpxnvsui4mjj8z3/MIMARKS_Data_combined.csv?raw=1",
+#                 destfile = mimarks_path)
+# samdf <- read.csv(mimarks_path, header = TRUE)
+samdf <- read.csv("/data/projects/glyphosate/analysis/metadata/meta_dna_water.csv",
+					header = TRUE, row.names = 1)
+all(rownames(seqtab) %in% rownames(samdf)) # TRUE
+				
+ps_water_dna <- phyloseq(tax_table(taxtab), sample_data(samdf),
+                 otu_table(seqtab, taxa_are_rows = FALSE),phy_tree(fitGTR$tree))
+
+# To begin, create a table of read counts for each e.g. Genus present in the dataset.
+
+# Show available ranks in the dataset
+rank_names(ps_water_dna)
+
+## [1] "Kingdom" "Phylum" "Class" "Order" "Family" "Genus"
+
+# Create table, number of features for each phyla
+table(tax_table(ps_water_dna)[, "Phylum"], exclude = NULL)
+
+# Compute prevalence of each feature, store as data.frame
+prevdf = apply(X = otu_table(ps_water_dna),
+                 MARGIN = ifelse(taxa_are_rows(ps_water_dna), yes = 1, no = 2),
+                 FUN = function(x){sum(x > 0)})
+# Add taxonomy and total read counts to this data.frame
+prevdf = data.frame(Prevalence = prevdf,
+                      TotalAbundance = taxa_sums(ps_water_dna),
+                      tax_table(ps_water_dna))
+					  
+plyr::ddply(prevdf, "Phylum", function(df1){cbind(mean(df1$Prevalence),sum(df1$Prevalence))})
+
+# how to get an OTU table? 
+# https://rdrr.io/bioc/phyloseq/man/otu_table-methods.html
+# Important informationh here! https://joey711.github.io/phyloseq/import-data.html
+
+otu_table(ps_water_dna) # without taxonomy, but actual sequence as header
+table_water_dna <- otu_table(ps_water_dna)
+write.csv(table_water_dna, file = "/data/projects/glyphosate/analysis/dada2/otu_table_water_dna.csv")
