81d1de58ebd3e6bfe090c02b7086bd44ad1d3fa8
RJ333 <35432752+RJ333@users.noreply.github.com>
2018-11-06 18:12:28 +0100

    #51 #55 #57 clean up mothur scripts

--- 16S_analysis_mothur/01_mothur_workflow_1395.h
+++ 16S_analysis_mothur/01_mothur_workflow_1395.h
@@ -1,18 +1,25 @@
-### bullet list with each step of mothur pipeline used
+# this script uses mothur interactively! It is not designed to run on its own
 
-# the first steps are very complicated: the mothur default path are not correctly pasted for the
-# output folder, therefore you can't have the stability.files in the output folder
-# generating the stability.files in the first place sets a new input directory
-# the stability.files later needs to be adjusted manually, as the reads contain the whole path
-# when the stability.files is adjusted and back in the folder, where the original reads are, you
+# the first steps are a little complicated: 1) the mothur default path are not 
+# correctly pasted for the output folder, therefore you can't have the 
+# stability.files in the output folder
+
+# 2) generating the stability.files in the first place sets a new input directory
+# the stability.files later needs to be adjusted manually, as the reads 
+# contain the whole path
+
+# when the stability.files is adjusted and back in the folder, 
+# where the original reads are, you
 # can continue 
 
 # this still requires a lot of playing around, not final yet
 
+# mothur does not like line breaks, even with \
+
 # You need to set the input and output in the same command. 
 # Mothur clears the the values that are not set when set.dir is run.
 # mothur > set.dir(input=../data/MiSeq_SOP, output=../analyses/)
-# set output dir
+# set input dir for raw reads and output dir for all further files
 set.dir(input = /data/projects/glyphosate/reads/mothur_processed, output = /data/projects/glyphosate/reads/mothur_processed)
 set.logfile(name = glyphosate_start_1395.log)
 
@@ -20,7 +27,7 @@ set.logfile(name = glyphosate_start_1395.log)
 # in this version, the stability file needs to be adjusted manually
 # from local shell
 scp -i ~/.ssh/denbi.key centos@193.196.20.111:/data/projects/glyphosate/reads/mothur_processed/stability.files /mnt/d/mothur1395/
-#adjusting e.g. in excel
+# adjusting e.g. in excel
 scp -i ~/.ssh/denbi.key /mnt/d/mothur1395/stability2.files.txt centos@193.196.20.111:/data/projects/glyphosate/reads/mothur_processed/
 
 # back on VM
@@ -29,29 +36,27 @@ mv /data/projects/glyphosate/reads/mothur_processed/stability2.files.txt /data/p
 make.file(inputdir = /data/projects/glyphosate/reads/raw_reads_16S/, type = gz)
 
 # make.file resets the directories, so reset them too!
-
 set.dir(input = /data/projects/glyphosate/reads/raw_reads_16S/, output = /data/projects/glyphosate/reads/mothur_processed)
 make.contigs(file = stability.files, processors = 28)
+
+# now we don't use the folder of the raw reads anymore
 set.dir(input = /data/projects/glyphosate/reads/mothur_processed, output = /data/projects/glyphosate/reads/mothur_processed)
-# 1.40.5, gz-files: It took 56538 secs to process 40476806 sequences.
-# 1.39.5, gz-files: It took 3372 secs to process 41041339 sequences. # but forgot to adjust stability.files, now it is one huge group
 summary.seqs(fasta = stability.trim.contigs.fasta, processors = 28)
 count.groups(group = stability.contigs.groups)
 
-# file oligo.txt copied from Lars Möller, 341f-805r as would be described in paper-style (805r not reversed-complementary)
+# file oligo.txt copied from Lars Möller, 
+# sequence of 341f-805r as would be described in paper-style 
+# --> (805r not reversed-complementary)
 trim.seqs(fasta = stability.trim.contigs.fasta, oligos = oligo.txt, processors = 28)	
-
-# It took 1128 secs to trim 41418035 sequences.
 
 # check quality of primer-trimmed reads
 summary.seqs(fasta = stability.trim.contigs.trim.fasta, processors = 28)
+# and the removed reads
 summary.seqs(fasta = stability.trim.contigs.scrap.fasta, processors = 28)
 
 # we have to create a new group file now
 list.seqs(fasta = stability.trim.contigs.trim.fasta)
 get.seqs(accnos = stability.trim.contigs.trim.accnos, group = stability.contigs.groups)
-
-# mothur does not like line breaks, even with \
 
 # removes(!) sequences which do not apply to the defined parameters from dataset 
 # (can be found in the accnos file)
@@ -60,7 +65,8 @@ screen.seqs(fasta = stability.trim.contigs.trim.fasta, group = stability.contigs
 # optional, summarises number of sequences per group (eg sample) 
 count.groups(group = stability.contigs.pick.good.groups)
 
-# looks for unique sequences and merges identical sequences under one reference unique sequence
+# looks for unique sequences and merges identical sequences 
+# under one reference unique sequence
 unique.seqs(fasta = stability.trim.contigs.trim.good.fasta)
 
 # creates a table with the unique sequences in each group 
@@ -69,9 +75,8 @@ unique.seqs(fasta = stability.trim.contigs.trim.good.fasta)
 count.seqs(name = stability.trim.contigs.trim.good.names, group = stability.contigs.pick.good.groups)
 summary.seqs(fasta = stability.trim.contigs.trim.good.unique.fasta, count = stability.trim.contigs.trim.good.count_table, processors = 28)
 
-# I copied the pcr.seqs and silva files from Lars Möller, as he used the same primers.
-# should still be checked again with SINA
-
+# I copied the pcr.seqs and silva files from Lars Möller, 
+# as he used the same primers.
 pcr.seqs(fasta = /data/db/silva.seed_v132.align, start = 6387, end = 23442, keepdots = F, processors = 28)
 	
 # trims the curated sequences of the reference database to a certain section 
@@ -83,10 +88,7 @@ pcr.seqs(fasta = /data/db/silva.seed_v132.align, start = 6387, end = 23442, keep
 
 # we use seed_v132 to align, this is the reduced database
 align.seqs(fasta = stability.trim.contigs.trim.good.unique.fasta, reference = silva.seed_v132.pcr.align, processors = 28)
-
 summary.seqs(fasta = stability.trim.contigs.trim.good.unique.align, count = stability.trim.contigs.trim.good.count_table, processors = 28)
-####### left off here
-
 
 # again removing sequences that dont match 
 # -> if Start or End do not fit or NBases is off
@@ -137,16 +139,15 @@ classify.seqs(fasta = stability.trim.contigs.trim.good.unique.good.filter.unique
 # removing phyla/lineages that we dont want in our dataset 
 # (e.g. Eukaryota, mitochondria etc because the primers are not 
 # supposed to match those)
-
-# remove.lineage(fasta = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.pick.fasta, count = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.denovo.vsearch.pick.count_table, taxonomy = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.pick.nr_v132.wang.taxonomy, taxon = Chloroplast-Mitochondria-unknown-Archaea-Eukaryota)
-# keeping Archea
 remove.lineage(fasta = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.pick.fasta, count = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.denovo.vsearch.pick.count_table, taxonomy = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.pick.nr_v132.wang.taxonomy, taxon = Chloroplast-Mitochondria-unknown-Eukaryota)
 summary.seqs(fasta = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.pick.pick.fasta, count=stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.denovo.vsearch.pick.pick.count_table, processors = 28)
 
 ##### OTU picking based on 98% similarity 
 # (memory intensive, performed on phy-2 with 1.39.5)
 
-# store private key at bio48/49, adjust to chmod 600, use to copy data from denbi-cloud to bio-48 
+# store private key at bio48/49, adjust to chmod 600, 
+# use to copy data from denbi-cloud to bio-48 
+
 # on bio-48:
 cd /data/projects/glyphosate/analysis_16S/mothur_1_39_5
 scp -i /data/Rene/ssh/denbi.key centos@193.196.20.111:/data/projects/glyphosate/reads/mothur_processed/stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.pick.pick.fasta .
@@ -163,11 +164,6 @@ cluster.split(fasta = stability.trim.contigs.trim.good.unique.good.filter.unique
 
 #################### this is where I stopped
 
-# missing part "unique_list" is due to new version, not a problem
-
 make.shared(list = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.pick.pick.opti_mcc.unique_list.list, count = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.denovo.vsearch.pick.pick.count_table, label = 0.02)
 # (label not important here?)
 # a shared file that contains all the information on which OTU was found how many times in which group (sample)
-
--> Output File Names:
-stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.pick.pick.opti_mcc.unique_list.shared		
