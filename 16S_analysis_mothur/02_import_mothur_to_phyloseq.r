9fd649f31d4b7a94e5ec659a354316cc813d616a
RJ333 <35432752+RJ333@users.noreply.github.com>
2018-11-14 18:50:06 +0100

    #58 #59 generated OTU seqs from mothur and imported to phyloseq

--- 16S_analysis_mothur/02_import_mothur_to_phyloseq.r
+++ 16S_analysis_mothur/02_import_mothur_to_phyloseq.r
@@ -22,6 +22,9 @@ if(any(!.inst)) {
 # Load packages into session, and print package version
 sapply(c(.cran_packages, .bioc_packages), require, character.only = TRUE)
 
+# when you combine different objects such as tax and otu table,
+# phyloseq performs an inner joint!
+
 # import mothur output into phyloseq
 mothur_ps <- import_mothur(mothur_list_file = NULL, 
   mothur_group_file = NULL,
@@ -36,8 +39,22 @@ rank_names(mothur_ps)
 # adjust taxonomy header
 colnames(tax_table(mothur_ps)) <- c(k = "Kingdom", p = "Phylum", c= "Class", 
 									o = "Order", f = "Family", g = "Genus" )
-  
- 
+									
+# when you combine different objects such as otu and meta table,
+# phyloseq performs an inner joint!
+
+# read meta data, turn into phyloseq object, merge with existing ps object									
+metafile <- read.delim("/data/projects/glyphosate/analysis/metadata/all_samples_with_meta.tsv", row.names = 1, header = TRUE)
+metafile2 <- sample_data(metafile)
+
+# read OTU representative sequences, for generation of file check gitlab #59
+OTU_seqs <- readDNAStringSet(file = "OTU_reps.fasta", format = "fasta",
+               nrec = -1L, skip = 0L, seek.first.rec = FALSE, use.names = TRUE)
+
+mothur_ps2 <- merge_phyloseq(mothur_ps, metafile2, refseq(OTU_seqs))
+
+mothur_ps3 <- filter_taxa(mothur_ps, function (x) {sum(x > 0) > 1}, prune=TRUE)
+
 # this changes the header from the actual sequence to Seq_001, Seq_002 etc
 taxa_names(mothur_ps)
 
@@ -51,8 +68,8 @@ taxa_names(mothur_ps)
 
 
 # generate a tree
-seqs <- getSequences(seqtab)
-names(seqs) <- seqs # This propagates to the tip labels of the tree
+seqs <- getSequences(OTU_seqs)
+#names(seqs) <- seqs # This propagates to the tip labels of the tree
 alignment <- AlignSeqs(DNAStringSet(seqs), anchor = NA)
 
 # phangorn package: we first construct a neighbor-joining tree,
