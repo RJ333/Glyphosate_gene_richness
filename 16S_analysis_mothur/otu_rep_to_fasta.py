92fa7f7a784952ba24116727ce2d9e20c1270f63
René Janßen <rene.janssen@io-warnemuende.de>
2018-11-21 11:22:15 +0100

    #59 added main() function

--- 16S_analysis_mothur/otu_rep_to_fasta.py
+++ 16S_analysis_mothur/otu_rep_to_fasta.py
@@ -9,18 +9,6 @@ therefore included in phyloseqs refseq() slot
 import argparse
 import re
 
-parser = argparse.ArgumentParser()
-
-parser.add_argument("-i", "--input_file", dest = "input_file", type = str,
-					default = None, required = True, action = "store",
-                    help = "input file (get.oturep() fasta file from mothur)")
-parser.add_argument("-o", "--output_file", dest = "output_file", type = str,
-					default = None, required = True, action = "store",
-                    help = "output file")
-args = parser.parse_args()
-
-seq_gap_symbol = [".", "-"]
-
 def clean_fasta_alignment(line, symbols):
 	"""
 	the for loop deletes the gap symbols in each fasta line.
@@ -36,12 +24,28 @@ def clean_fasta_alignment(line, symbols):
         line = line.rstrip().replace(symbol, "")
     return line
 
-with open(args.input_file) as open_fasta_line, open(args.output_file, "w") as open_output_file:
-    for line in open_fasta_line:
-        if line.startswith(">"):
-			# line.split selects the part of the header to look for regex
-            fasta_header = re.match(r"(.+?)\|", line.split()[1]).group(1)
-            open_output_file.write(">" + fasta_header + "\n")
-        else:
-            seq = clean_fasta_alignment(line, seq_gap_symbol)
-            open_output_file.write(seq + "\n")
+def main():	
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-i", "--input_file", dest = "input_file", type = str,
+						default = None, required = True, action = "store",
+	                    help = "input file (get.oturep() fasta file from mothur)")
+	parser.add_argument("-o", "--output_file", dest = "output_file", type = str,
+						default = None, required = True, action = "store",
+	                    help = "output file")
+	args = parser.parse_args()
+
+	seq_gap_symbol = [".", "-"]
+
+	with open(args.input_file) as open_fasta_line, open(args.output_file, "w") as open_output_file:
+	    for line in open_fasta_line:
+	        if line.startswith(">"):
+				# line.split selects the part of the header to look for regex
+	            fasta_header = re.match(r"(.+?)\|", line.split()[1]).group(1)
+	            open_output_file.write(">" + fasta_header + "\n")
+	        else:
+	            seq = clean_fasta_alignment(line, seq_gap_symbol)
+	            open_output_file.write(seq + "\n")
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
