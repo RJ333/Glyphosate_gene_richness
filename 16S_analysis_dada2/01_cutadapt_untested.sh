8d8c9ada7c4f53e4df68da01b5fc82d9de4fece6
RJ333 <35432752+RJ333@users.noreply.github.com>
2018-12-06 13:46:35 +0100

    Update 01_cutadapt_untested.sh

--- 16S_analysis_dada2/01_cutadapt_untested.sh
+++ 16S_analysis_dada2/01_cutadapt_untested.sh
@@ -2,9 +2,11 @@
 # this script assumes that f and r reads are still separated
 # and primer set 341f-805r (V3-V4)
 
-input=$1 # argument passing has not been tested yet
+# define input and output dir
+input=$1
 output=$2
 
+# run cutadapt with fixed primer sequences
 for xy in $1/*R*.gz
 do
   cutadapt -j 20 -g CCTACGGGNGGCWGCAG -g GACTACHVGGGTATCTAATCC -o $2/cut_${xy} ${xy} 
