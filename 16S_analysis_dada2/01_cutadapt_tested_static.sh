5aa700f156ae08278729d746aa47603c5cc343dd
RJ333 <35432752+RJ333@users.noreply.github.com>
2018-12-07 13:37:19 +0100

    added comment and changed variable name

--- 16S_analysis_dada2/01_cutadapt_tested_static.sh
+++ 16S_analysis_dada2/01_cutadapt_tested_static.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-for xy in *R*.gz
+
+# this script removes the -g specified primers from all read libraries in a dir
+# it assumes that forward and reverse reads are separate
+# -j is the amount of processors
+# output is prepended by "cut_"
+
+for library in *R*.gz
 do
-  cutadapt -j 20 -g CCTACGGGNGGCWGCAG -g GACTACHVGGGTATCTAATCC -o /data/projects/glyphosate/reads/reads_16S_cutadapt/water_dna/cut_${xy} $xy
+  cutadapt -j 20 -g CCTACGGGNGGCWGCAG -g GACTACHVGGGTATCTAATCC -o /data/projects/glyphosate/reads/reads_16S_cutadapt/water_dna/cut_${library} $library
 done
