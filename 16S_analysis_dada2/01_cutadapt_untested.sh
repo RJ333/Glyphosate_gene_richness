55d18a96fdd107b375cc4ed0ed606910fba4f17e
RJ333 <35432752+RJ333@users.noreply.github.com>
2018-12-07 15:57:33 +0100

    adding argument parsing to cutadapt script

--- 16S_analysis_dada2/01_cutadapt_untested.sh
+++ 16S_analysis_dada2/01_cutadapt_untested.sh
@@ -1,13 +1,82 @@
 #!/bin/bash
-# this script assumes that f and r reads are still separated
-# and primer set 341f-805r (V3-V4)
 
-# define input and output dir
-input=$1
-output=$2
+: 'multiline comment in bash
+This script is a wrapper around cutadapt to remove oligos such as primers.
+It assumes that forward and reverse reads are separate. The output is 
+prepended by "cut_"
+'
 
-# run cutadapt with fixed primer sequences
-for xy in $1/*R*.gz
+: '
+Args:
+    -i|--input 			: the input path, containing gzipped reads
+	-o|--output 		: the output path
+	-c|--cores 			: the number of cores
+	-p|--primersequence : the oligonucleotide sequence to be removed from the beginning of the read!
+	-d|--direction 		: choose the reads to work on: "R1" for forward, "R2" is reverse, "R" for both directions
+       --default 		: I dont know what it does, but I will see whether it is required
+
+Return:
+	the selected read libraries without the specified sequence, gzipped.
+'
+
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
 do
-  cutadapt -j 20 -g CCTACGGGNGGCWGCAG -g GACTACHVGGGTATCTAATCC -o $2/cut_${xy} ${xy} 
+key="$1"
+
+case $key in
+    -i|--input)
+    INPUT="$2"
+    shift # past argument
+    shift # past value
+    ;;
+	-o|--output)
+    OUTPUT="$2"
+    shift # past argument
+    shift # past value
+    ;;
+	-c|--cores)
+    CORES="$2"
+    shift # past argument
+    shift # past value
+    ;;
+	-p|--primersequence)
+    PRIMER_SEQUENCE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+	-d|--direction)
+    DIRECTION="$2"
+    shift # past argument
+    shift # past value
+    ;;
+	--default)
+    DEFAULT=YES
+    shift # past argument
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
 done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+echo INPUT = "${INPUT}"
+echo OUTPUT = "${OUTPUT}"
+echo CORES = "${CORES}"
+echo PRIMER_SEQUENCE = "${PRIMER_SEQUENCE}"
+echo DIRECTION = "${DIRECTION}"
+
+# run cutadapt
+for library in ${INPUT}/*${DIRECTION}*.gz
+do
+  cutadapt -j $CORES -g $PRIMER_SEQUENCE -o ${OUTPUT}/cut_${library} $library 
+done
+
+# return unused arguments (I guess?)
+if [[ -n $1 ]]; then
+    echo "Last line of file specified as non-opt/last argument:"
+    tail -1 "$1"
+fi
