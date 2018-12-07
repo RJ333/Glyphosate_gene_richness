f13eafa86cffcd854cfc898d77213f00b6d9e326
RJ333 <35432752+RJ333@users.noreply.github.com>
2018-12-07 11:54:53 +0100

    improved commentation on mothur workflow

--- 16S_analysis_mothur/01_mothur_workflow_1395.h
+++ 16S_analysis_mothur/01_mothur_workflow_1395.h
@@ -40,24 +40,22 @@ summary.seqs(fasta = stability.trim.contigs.trim.fasta, processors = 28)
 # and the stats of the removed reads
 summary.seqs(fasta = stability.trim.contigs.scrap.fasta, processors = 28)
 
-# we have to create a new group file now
+# update the files after primer removal
 list.seqs(fasta = stability.trim.contigs.trim.fasta)
 get.seqs(accnos = stability.trim.contigs.trim.accnos, group = stability.contigs.groups)
 
-# removes(!) sequences which do not apply to the defined parameters from dataset 
-# (can be found in the accnos file)
+# perform quality trimming on the sequences 
 screen.seqs(fasta = stability.trim.contigs.trim.fasta, group = stability.contigs.pick.groups, maxambig = 0, maxhomop = 8, maxlength = 450, minlength = 390, processors = 28) 
 
 # optional, summarises number of sequences per group 
 count.groups(group = stability.contigs.pick.good.groups)
 
-# looks for unique sequences and merges identical sequences 
+# looks for unique sequences and dereplicates identical sequences 
 # under one reference unique sequence
 unique.seqs(fasta = stability.trim.contigs.trim.good.fasta)
 
-# creates a table with the unique sequences in each group 
-# and the number of times each unique sequence 
-# is found in total and per group (sample)-> important for abundance data!
+# creates a table with the unique sequences in each group and how often each unique sequence 
+# is found in total and per group (sample) -> important for abundance data!
 count.seqs(name = stability.trim.contigs.trim.good.names, group = stability.contigs.pick.good.groups)
 summary.seqs(fasta = stability.trim.contigs.trim.good.unique.fasta, count = stability.trim.contigs.trim.good.count_table, processors = 28)
 
@@ -66,22 +64,19 @@ summary.seqs(fasta = stability.trim.contigs.trim.good.unique.fasta, count = stab
 # If you don't know the start/end parameters of your primers, use SINA to identify the regions
 pcr.seqs(fasta = /data/db/silva.seed_v132.align, start = 6387, end = 23442, keepdots = F, processors = 28)
 	
-# we use seed_v132 to align, this is the reduced SILVA database
+# we use seed_v132 (seed is reduced SILVA database) to align our sequences 
 align.seqs(fasta = stability.trim.contigs.trim.good.unique.fasta, reference = silva.seed_v132.pcr.align, processors = 28)
 summary.seqs(fasta = stability.trim.contigs.trim.good.unique.align, count = stability.trim.contigs.trim.good.count_table, processors = 28)
 
-# again removing sequences that dont match 
-# -> if Start or End do not fit or NBases is off
+# again removing sequences that don't match start or end values (based on previous summary.seqs()-output)
 screen.seqs(fasta = stability.trim.contigs.trim.good.unique.align, count = stability.trim.contigs.trim.good.count_table, summary = stability.trim.contigs.trim.good.unique.summary, start = 41, end = 17053, processors = 28)
 summary.seqs(fasta = stability.trim.contigs.trim.good.unique.good.align, count = stability.trim.contigs.trim.good.good.count_table)
 
-# removes gap characters within the sequences (e.g. '-') 
-# to condense file and save disk space
+# the aligned seqs contain gap characters (e.g. '-'), remove them to save disk space
 filter.seqs(fasta = stability.trim.contigs.trim.good.unique.good.align, vertical = T)
 
-# because after all the trimming and aligning, 
-# perhaps more redundant sequences can be found, 
-# thats why we use unique.seqs again
+# after trimming and aligning more redundant sequences might be found, 
+# so we dereplicate again
 unique.seqs(fasta = stability.trim.contigs.trim.good.unique.good.filter.fasta, count = stability.trim.contigs.trim.good.good.count_table)
 summary.seqs(fasta = stability.trim.contigs.trim.good.unique.good.filter.unique.fasta, count = stability.trim.contigs.trim.good.unique.good.filter.count_table)
 
@@ -93,32 +88,29 @@ summary.seqs(fasta = stability.trim.contigs.trim.good.unique.good.filter.unique.
 pre.cluster(fasta = stability.trim.contigs.trim.good.unique.good.filter.unique.fasta, count = stability.trim.contigs.trim.good.unique.good.filter.count_table, diffs = 2)
 summary.seqs(fasta = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.fasta, count = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.count_table)
 
-# now we want to detect chimeric sequences in our data set; 
+# detect chimeric sequences in our data set; 
 # dereplicate=true (t) means that if one sequence gets flagged as chimeric 
-# in one group, it is NOT automatically flagged as chimeric in other groups #
+# in one group, it is NOT automatically flagged as chimeric in other groups
 # (because it might be an abundant sequence in another group) 
-
-# I copied the vsearch bin from mothur executable into the conda environment
 chimera.vsearch(fasta = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.fasta, count = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.count_table, dereplicate = t, processors = 28)
 
 # optional: count seqs per sample 
 count.groups(count = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.denovo.vsearch.pick.count_table)
 
-#removing the chimeric sequences from our dataset (fasta file)
+# removing the chimeric sequences from our dataset (fasta file)
 remove.seqs(fasta = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.fasta, accnos = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.denovo.vsearch.accnos)
 summary.seqs(fasta = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.pick.fasta, count = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.denovo.vsearch.pick.count_table)
 
 # classifying the sequences (before OTU picking!) based on the 
 # complete SILVA reference set (non redundant?) 
 # cutoff = 85 (85%) means that everything gets classified 
-# as 'non-classified' when there not at least 85% probability (similarity?) 
+# as 'non-classified' below 85% probability (similarity?) 
 # to the next phylo level; try probs=F to remove bootstrap values from 
 # taxonomy
 classify.seqs(fasta = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.pick.fasta, count = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.denovo.vsearch.pick.count_table, reference = /data/db/silva.nr_v132.align, taxonomy = /data/db/silva.nr_v132.tax, cutoff = 85)
 
-# removing phyla/lineages that we dont want in our dataset 
-# (e.g. Eukaryota, mitochondria etc because the primers are not 
-# supposed to match those)
+# removing phyla/lineages that we dont want in our dataset (e.g. Eukaryota, mitochondria) 
+# or should not appear because the primers are not supposed to match those
 remove.lineage(fasta = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.pick.fasta, count = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.denovo.vsearch.pick.count_table, taxonomy = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.pick.nr_v132.wang.taxonomy, taxon = Chloroplast-Mitochondria-unknown-Eukaryota)
 summary.seqs(fasta = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.pick.pick.fasta, count=stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.denovo.vsearch.pick.pick.count_table, processors = 28)
 
@@ -128,7 +120,7 @@ summary.seqs(fasta = stability.trim.contigs.trim.good.unique.good.filter.unique.
 # check the README.md if you want to transfer the files to phy-2, which has more RAM
 cluster.split(fasta = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.pick.pick.fasta, count = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.denovo.vsearch.pick.pick.count_table, taxonomy = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.pick.nr_v132.wang.pick.taxonomy, splitmethod = classify, taxlevel = 4, cutoff = 0.03, processors = 10)
 
-# this is the count table with absolute values (check whether the file names include "0.02" or similar)
+# this is the count table with absolute values (check whether the file names include the label "0.02" or similar)
 make.shared(list = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.pick.pick.opti_mcc.unique_list.0.02.list, count = stability.trim.contigs.trim.good.unique.good.filter.unique.precluster.denovo.vsearch.pick.pick.count_table)
 
 # now we can classify our OTUs
