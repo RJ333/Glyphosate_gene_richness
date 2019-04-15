e4470acab3246f6d32e833043e832cd6a9277d9a
RJ333 <35432752+RJ333@users.noreply.github.com>
2019-04-15 14:30:29 +0200

    changed mg per L to micromol, reformatting line breaks

--- 16S-analysis_phyloseq/streamlined_R_code_for_publication.r
+++ 16S-analysis_phyloseq/streamlined_R_code_for_publication.r
@@ -397,39 +397,30 @@ current_plot <- ggplot(data = current_otu_data,
 cell_counts_glyph_0 <- subset(cell_counts_glyph, new_day >= -7)
 
 cell_counts_glyph_plot <- ggplot(cell_counts_glyph_0, aes(x = new_day,
-                                colour = treatment,
-                                linetype = treatment,
-                                group = treatment)) +
-	geom_errorbar(aes(ymin = cells_ml - cells_se,
-                      ymax = cells_ml + cells_se),
-                  linetype = 1, width = 2, size = 1.2, alpha = 0.7) +
-	geom_errorbar(aes(ymin = (glyph_mg_L - glyph_se) * 2200000,
-                      ymax = (glyph_mg_L + glyph_se) * 2200000),
-                  linetype = 1, width = 1, size = 1.0, alpha = 0.6) +
-    geom_line(aes(y = cells_ml,
-                  group = treatment,
-                  colour = treatment),
-              linetype = 1, size = 2, alpha = 0.8) +
-	geom_point(aes(y = glyph_theor * 2200000,
-                   shape = "glyph"),
-               alpha = 0.5, size = 5) +
-	geom_point(aes(y = glyph_mg_L * 2200000,
-                   shape = "glyph_deg"),
-               alpha = 0.7, size = 4) +
-
-	geom_vline(aes(xintercept = 0), linetype = "dashed", size = 1.5, alpha = 0.5)+
-
-	scale_y_continuous(label =  function(x) {ifelse(x == 0, "0", parse(text = gsub("[+]", "", gsub("e", " %*% 10^", scientific_format()(x)))))},
-        sec.axis = sec_axis(~ . / 2200000, name = expression(paste("Glyphosate concentration  ", bgroup("[",mg~L^{-1},"]")))))+
-	scale_colour_manual(values = c("control" = "grey70", "glyph" = "black"),
-                        name = "Microcosm",
-                        breaks = c("control", "glyph"),
-                        labels = c("Control", "Treatment"))+
-	scale_shape_manual(values = c("glyph" = 2, "glyph_deg" = 17),
-                       name = "Glyphosate decrease by",
-                       breaks = c("glyph", "glyph_deg"),
-                       labels = c("calculated\ndilution", "measured\nconcentration"))+
-	scale_x_continuous(breaks = scales::pretty_breaks(n = 10))+
+  colour = treatment, linetype = treatment, group = treatment)) +
+  geom_errorbar(aes(ymin = cells_ml - cells_se, ymax = cells_ml + cells_se),
+    linetype = 1, width = 2, size = 1.2, alpha = 0.7) +
+  geom_errorbar(aes(ymin = (glyph_micromol - glyph_se_micromol) * 400000,
+    ymax = (glyph_micromol + glyph_se_micromol) * 400000), linetype = 1, 
+    width = 1, size = 1.0, alpha = 0.6) +
+  geom_line(aes(y = cells_ml, group = treatment, colour = treatment), linetype = 1, 
+    size = 2, alpha = 0.8) +
+  geom_point(aes(y = glyph_theor_micromol * 400000, shape = "glyph"), alpha = 0.5, 
+    size = 5) +
+  geom_point(aes(y = glyph_micromol * 400000, shape = "glyph_deg"), alpha = 0.7, 
+    size = 4) +
+  geom_vline(aes(xintercept = 0), linetype = "dashed", size = 1.5, alpha = 0.5) +
+  scale_y_continuous(label =  function(x) {ifelse(x == 0, "0", parse(text = 
+    gsub("[+]", "", gsub("e", " %*% 10^", scientific_format()(x)))))},
+    sec.axis = sec_axis(~ . / 400000, name = expression(paste(
+      "Glyphosate concentration  ", bgroup("[",µM,"]"))))) +
+  scale_colour_manual(values = c("control" = "grey70", "glyph" = "black"),
+    name = "Microcosm", breaks = c("control", "glyph"), 
+    labels = c("Control", "Treatment")) +
+  scale_shape_manual(values = c("glyph" = 2, "glyph_deg" = 17), 
+    name = "Glyphosate decrease by", breaks = c("glyph", "glyph_deg"),
+    labels = c("calculated\ndilution", "measured\nconcentration")) +
+  scale_x_continuous(breaks = scales::pretty_breaks(n = 10)) +
     theme_bw() +
 	theme(panel.grid.major = element_line(colour = NA, size = 0.2),
           panel.grid.minor = element_line(colour = NA, size = 0.5),
@@ -438,8 +429,8 @@ cell_counts_glyph_plot <- ggplot(cell_counts_glyph_0, aes(x = new_day,
           axis.text = element_text(size = 18),
           legend.title=element_text(size = 14),
           legend.text=element_text(size = 12)) +
-	labs(x = "Days",
-		 y = expression(paste("Total cell count  ",bgroup("[",cells~mL^{-1},"]"))))
+  labs(x = "Days", y = expression(paste("Total cell count  ",
+    bgroup("[",cells~mL^{-1},"]"))))
 
 ggsave(cell_counts_glyph_plot, file = paste(plot_path, "Figure_1_cellcounts_glyph.png",
                                             sep = ""),
