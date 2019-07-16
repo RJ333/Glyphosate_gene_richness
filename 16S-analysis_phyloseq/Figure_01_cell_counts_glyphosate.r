984733bb96e24bd7f8edaa47d590ca34188efab9
RJ333 <35432752+RJ333@users.noreply.github.com>
2019-07-16 14:45:31 +0200

    #81 adjusted figure 1

--- 16S-analysis_phyloseq/Figure_01_cell_counts_glyphosate.r
+++ 16S-analysis_phyloseq/Figure_01_cell_counts_glyphosate.r
@@ -19,18 +19,18 @@ cell_counts_glyph_plot <- ggplot(cell_counts_glyph_0, aes(x = new_day,
     linetype = 1, width = 2, size = 1.2, alpha = 0.7) +
   geom_errorbar(aes(ymin = (glyph_micromol - glyph_se_micromol) * 400000,
     ymax = (glyph_micromol + glyph_se_micromol) * 400000), linetype = 1, 
-    width = 1, size = 1.0, alpha = 0.6) +
+    width = 3, size = 0.7, alpha = 0.8) +
   geom_line(aes(y = cells_ml, group = treatment, colour = treatment), 
-    linetype = 1, size = 2, alpha = 0.8) +
+    linetype = 1, size = 1.5, alpha = 0.8) +
   geom_point(aes(y = glyph_theor_micromol * 400000, shape = "glyph"), 
-    alpha = 0.5, size = 5) +
+    alpha = 0.5, size = 4) +
   geom_point(aes(y = glyph_micromol * 400000, shape = "glyph_deg"), 
-    alpha = 0.7, size = 4) +
+    alpha = 0.7, size = 3) +
   geom_vline(aes(xintercept = 0), linetype = "dashed", size = 1.5, alpha = 0.5) +
   scale_y_continuous(label =  function(x) {ifelse(x == 0, "0", parse(text = 
     gsub("[+]", "", gsub("e", " %*% 10^", scientific_format()(x)))))},
-    sec.axis = sec_axis(~ . / 400000, name = expression(paste(
-      "Glyphosate concentration  ", bgroup("[",µM,"]"))))) +
+    sec.axis = sec_axis(~ . / 400000, name = expression(bold(paste(
+      "Glyphosate  ", bgroup("[",µM,"]")))))) +
   scale_colour_manual(values = c("control" = "grey70", "glyph" = "black"),
     name = "Microcosm", breaks = c("control", "glyph"), 
     labels = c("Control", "Treatment")) +
@@ -38,16 +38,19 @@ cell_counts_glyph_plot <- ggplot(cell_counts_glyph_0, aes(x = new_day,
     name = "Glyphosate decrease by", breaks = c("glyph", "glyph_deg"),
     labels = c("calculated\ndilution", "measured\nconcentration")) +
   scale_x_continuous(breaks = scales::pretty_breaks(n = 10)) +
-    theme_bw() +
-	theme(panel.grid.major = element_line(colour = NA, size = 0.2),
+  labs(x = "Days", y = expression(bold(paste("Total cell count  ",
+    bgroup("[",cells~mL^{-1},"]"))))) +
+  theme_bw() +
+    theme(panel.grid.major = element_line(colour = NA, size = 0.2),
       panel.grid.minor = element_line(colour = NA, size = 0.5),
-      axis.title = element_text(size = 20, face = "bold"),
-      axis.title.y = element_text(angle = 90, vjust = 1),
-      axis.text = element_text(size = 18),
-      legend.title=element_text(size = 14),
-      legend.text=element_text(size = 12)) +
-  labs(x = "Days", y = expression(paste("Total cell count  ",
-    bgroup("[",cells~mL^{-1},"]"))))
+      axis.title = element_text(size = 16, face = "bold"),
+      axis.title.y = element_text(angle = 90, vjust = 1, face = "bold"),
+      axis.text = element_text(size = 14),
+      legend.title = element_text(size = 12, face = "bold"),
+      legend.text = element_text(size = 10),
+      strip.text.x = element_text(size = 16, colour = "black", face = "bold")) +
+    theme(legend.position = "bottom", legend.direction = "vertical")
 
 ggsave(cell_counts_glyph_plot, file = paste(plot_path, 
-  "Figure_1_cellcounts_glyph.png", sep = ""), width = 14, height = 10)
+  "Figure_1_cellcounts_glyph.pdf", sep = ""),
+  device = "pdf", width = 18, height = 12, dpi = 300, unit = "cm")
