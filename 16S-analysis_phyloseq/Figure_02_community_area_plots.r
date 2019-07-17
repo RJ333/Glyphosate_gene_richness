14e442f3f30f22236b7c2f4a65decf39d654b8da
RJ333 <35432752+RJ333@users.noreply.github.com>
2019-07-17 10:58:30 +0200

    #81 #80 updated Figure 2 and SI_4 for journal

--- 16S-analysis_phyloseq/Figure_02_community_area_plots.r
+++ 16S-analysis_phyloseq/Figure_02_community_area_plots.r
@@ -96,25 +96,29 @@ levels(order_sums_water$nucleic_acid) <- c("16S rRNA gene", "16S rRNA")
 # plot an array of 4 geom_areas
 water_areas <- ggplot(order_sums_water, aes(x = new_day, y = Abundance2, fill = order)) +
   geom_area(stat = "identity") +
-  geom_vline(aes(xintercept = 0), linetype = "dashed", size = 1.2) +
+  geom_vline(aes(xintercept = 0), linetype = "dashed", size = 1) +
   scale_fill_manual(breaks = levels(order_sums_water$order), values = fill_values_water) +
   guides(colour = FALSE, size = FALSE, width = FALSE, fill = guide_legend(ncol = 1,
-    keyheight = 1.5, label.theme = element_text(size = 15, face = "italic",
+    keyheight = 1.2, label.theme = element_text(size = 12, face = "italic",
     angle = 0), (title = NULL))) +
   scale_x_continuous(expand = c(0, 0), breaks = scales::pretty_breaks(n = 10)) +
   scale_y_continuous(expand = c(0, 0)) +
   theme_bw() +
-  theme(axis.text = element_text(size = 17),
-    axis.title = element_text(size = 20, face = "bold"),
-    legend.background = element_rect(fill = "grey90", linetype = "solid"),
-    panel.grid.major = element_line(colour = NA, size = 0.2),
+  theme(panel.grid.major = element_line(colour = NA, size = 0.2),
     panel.grid.minor = element_line(colour = NA, size = 0.5),
-    strip.text.x = element_text(size = 15, face = "bold")) +
+    axis.title = element_text(size = 16, face = "bold"),
+    axis.title.y = element_text(angle = 90, vjust = 1),
+    axis.text = element_text(size = 14),
+    legend.title = element_text(size = 14, face = "bold"),
+    legend.text = element_text(size = 12),
+    strip.text.x = element_text(size = 14, colour = "black", face = "bold"),
+    legend.background = element_rect(fill = "grey90", linetype = "solid")) +
   labs(x = "Days", y = "Relative abundance [%]") +
+ # theme(legend.position = "bottom", legend.direction = "horizontal") +
   facet_wrap(~ treatment + nucleic_acid, nrow = 2)
   
-ggsave(water_areas, file = paste(plot_path, "Figure_2_water_communities.png", sep = ""),
-  height = 10, width = 16)
+ggsave(water_areas, file = paste(plot_path, "Figure_2_water_communities.pdf", sep = ""),
+  device = "pdf", width = 26.0, height = 18, dpi = 300, unit = "cm")
   
 # get data per OTU, setting threshold for samples and clusters
 community_subset_biofilm <- droplevels(subset(mothur_ra_melt_mean, days > 40
@@ -178,22 +182,27 @@ levels(order_sums_biofilm$nucleic_acid) <- c("16S rRNA gene", "16S rRNA")
 # biofilm are plot for SI
 biofilm_areas <- ggplot(order_sums_biofilm, aes(x = new_day, y = Abundance2, fill = order)) +
   geom_area(stat = "identity") +
-  geom_vline(aes(xintercept = 0), linetype = "dashed", size = 1.2) +
+  geom_vline(aes(xintercept = 0), linetype = "dashed", size = 1) +
   scale_fill_manual(breaks = levels(order_sums_biofilm$order), values = fill_values_biofilm) +
-  guides(colour = FALSE, size = FALSE, width = FALSE, fill = guide_legend(ncol = 1,
-    keyheight = 1.5, label.theme = element_text(size = 15, face = "italic",
-    angle = 0), (title = NULL))) +
+  guides(color = FALSE) +
+  guides(size = FALSE) +
+  guides(width = FALSE) +
+  guides(fill = guide_legend(label.theme = element_text(size = 12, face = "italic",
+    angle = 0), ncol = 1, keyheight = 1.2, (title = NULL))) +
   scale_x_continuous(expand = c(0, 0), breaks = scales::pretty_breaks(n = 10)) +
   scale_y_continuous(expand = c(0, 0)) +
   theme_bw() +
-  theme(axis.text = element_text(size = 17),
-    axis.title = element_text(size = 20, face = "bold"),
-    legend.background = element_rect(fill = "grey90", linetype = "solid"),
-    panel.grid.major = element_line(colour = NA, size = 0.2),
+  theme(panel.grid.major = element_line(colour = NA, size = 0.2),
     panel.grid.minor = element_line(colour = NA, size = 0.5),
-    strip.text.x = element_text(size = 15, face = "bold")) +
+    axis.title = element_text(size = 16, face = "bold"),
+    axis.title.y = element_text(angle = 90, vjust = 1),
+    axis.text = element_text(size = 14),
+    legend.title = element_text(size = 14, face = "bold"),
+    legend.text = element_text(size = 12),
+    strip.text.x = element_text(size = 14, colour = "black", face = "bold"),
+    legend.background = element_rect(fill = "grey90", linetype = "solid")) +
   labs(x = "Days", y = "Relative abundance [%]") +
   facet_wrap(~ treatment + nucleic_acid, nrow = 2)
   
-ggsave(biofilm_areas, file = paste(plot_path, "SI_biofilm_communities.png", sep = ""),
-  height = 10, width = 16)
+ggsave(biofilm_areas, file = paste(plot_path, "SI_4_biofilm_communities.pdf", sep = ""),
+  device = "pdf", width = 26.0, height = 18, dpi = 300, unit = "cm")
